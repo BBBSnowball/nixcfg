04b1b67ea4365f414bf1940553b60dea26ddb677
Your Name <root@nixos>
2019-06-05 16:26:17 +0200

    initial commit - probably broken

---
+++ configuration.nix
@@ -0,0 +1,127 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  boot.loader.grub.device = "/dev/vda";
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+      consoleKeyMap = "de";
+      defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget vim htop tmux byobu
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  networking.interfaces.ens3.ipv4.addresses = [ {
+    address = "192.168.84.133";
+    prefixLength = 24;
+  } ];
+  networking.useDHCP = false;
+
+  networking.defaultGateway = "192.168.84.128";
+  networking.nameservers = [ "62.210.16.6" "62.210.16.7" ];
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.jane = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  # };
+  users.users.root.openssh.authorizedKeys.keys = [
+    #(builtins.readFile ./private/ssh-some-admin-key.pub)
+    (builtins.readFile ./private/ssh-laptop.pub)
+    (builtins.readFile ./private/ssh-dom0.pub)
+  ];
+
+  #headless = true;
+  sound.enable = false;
+  boot.vesa = false;
+  boot.loader.grub.splashImage = null;
+
+  systemd.services."serial-getty@ttyS0".enable = true;
+  boot.kernelParams = [ "console=ttyS0" ];
+
+  security.rngd.enable = false;
+
+  system.autoUpgrade.enable = true;
+
+  services.taskserver.enable = true;
+  services.taskserver.fqdn = builtins.readFile ./private/taskserver-fqdn.txt;
+  services.taskserver.listenHost = "::";
+  services.taskserver.organisations.snente.users = [ "snowball" "ente" ];
+
+  services.fstrim.enable = true;
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03"; # Did you read the comment?
+
+}
