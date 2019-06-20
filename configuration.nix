029e09057b35e327e78c0000bdff6c70dc6ba6dc
Your Name <root@nixos>
2019-06-20 06:31:43 +0200

    clean up

--- configuration.nix
+++ configuration.nix
@@ -594,81 +594,55 @@ in {
           };
           locations."/selfoss" = {
             root = "/var/lib/selfoss";
-            #index = "index.php";
             extraConfig = ''
-                        #location /selfoss/favicons/   { alias /var/lib/selfoss/data/favicons/; }                                                                             
-                        #location /selfoss/thumbnails/ { alias /var/lib/selfoss/data/thumbnails/; }                                                                           
-                        # regex matches win so make this a regex match
-                        location ~ ^/selfoss/favicons/(.*)   { alias /var/lib/selfoss/data/favicons/$1; }                                                                     
-                        location ~ ^/selfoss/thumbnails/(.*) { alias /var/lib/selfoss/data/thumbnails/$1; }                                                                   
+              # similar to nixos/modules/services/mail/roundcube.nix - well, not so similar anymore
+              location ~ ^/selfoss/?([?].*)?$ {
+                alias /var/lib/selfoss/index.php?$1;
+                fastcgi_pass unix:/run/phpfpm/my_selfoss_pool.sock;
+            
+                # We could include ${nginx}/conf/fastcgi_params but we need a different
+                # SCRIPT_FILENAME, SCRIPT_NAME and REQUEST_URI.
 
-                        # similar to nixos/modules/services/mail/roundcube.nix
-                        #FIXME only for index.php ?
-                        #location ~* \.php$ {
-                        location ~ ^/selfoss/?([?].*)?$ {
-                                alias /var/lib/selfoss/index.php?$1;
-                                #fastcgi_split_path_info ^(.+\.php)(/.+)$;
-                                fastcgi_pass unix:/run/phpfpm/my_selfoss_pool.sock;
-                                #include /nix/store/ljlr3i0j2lb4xb0m3m3752g8d9xfn645-nginx-1.14.2/conf/fastcgi_params;                                                        
-                                #include /nix/store/ljlr3i0j2lb4xb0m3m3752g8d9xfn645-nginx-1.14.2/conf/fastcgi.conf;                                                          
-                                #fastcgi_index index.php;
-                                #try_files /var/lib/selfoss/index.php /var/lib/selfoss/index.php;                                                                             
-                                #root /var/lib;
+                fastcgi_param SCRIPT_FILENAME /var/lib/selfoss/index.php;                                                                                     
+                fastcgi_param SCRIPT_NAME /selfoss/index.php;
+                fastcgi_param REQUEST_URI        /selfoss/$1;
 
-                                fastcgi_param SCRIPT_FILENAME /var/lib/selfoss/index.php;                                                                                     
-                                fastcgi_param SCRIPT_NAME /selfoss/index.php;
-                                fastcgi_param REQUEST_URI        /selfoss/$1;
+                fastcgi_param  QUERY_STRING       $query_string;
+                fastcgi_param  REQUEST_METHOD     $request_method;
+                fastcgi_param  CONTENT_TYPE       $content_type;
+                fastcgi_param  CONTENT_LENGTH     $content_length;
 
-                                fastcgi_param  QUERY_STRING       $query_string;
-                                fastcgi_param  REQUEST_METHOD     $request_method;
-                                fastcgi_param  CONTENT_TYPE       $content_type;
-                                fastcgi_param  CONTENT_LENGTH     $content_length;
+                #fastcgi_param  REQUEST_URI        $request_uri;
+                fastcgi_param  DOCUMENT_URI       $document_uri;
+                fastcgi_param  DOCUMENT_ROOT      $document_root;
+                fastcgi_param  SERVER_PROTOCOL    $server_protocol;
+                fastcgi_param  REQUEST_SCHEME     $scheme;
+                fastcgi_param  HTTPS              $https if_not_empty;
 
-                                #fastcgi_param  SCRIPT_NAME        $fastcgi_script_name;                                                                                      
-                                #fastcgi_param  REQUEST_URI        $request_uri;
-                                fastcgi_param  DOCUMENT_URI       $document_uri;
-                                fastcgi_param  DOCUMENT_ROOT      $document_root;
-                                fastcgi_param  SERVER_PROTOCOL    $server_protocol;
-                                fastcgi_param  REQUEST_SCHEME     $scheme;
-                                fastcgi_param  HTTPS              $https if_not_empty;
+                fastcgi_param  GATEWAY_INTERFACE  CGI/1.1;
+                fastcgi_param  SERVER_SOFTWARE    nginx/$nginx_version;                                                                                       
 
-                                fastcgi_param  GATEWAY_INTERFACE  CGI/1.1;
-                                fastcgi_param  SERVER_SOFTWARE    nginx/$nginx_version;                                                                                       
+                fastcgi_param  REMOTE_ADDR        $remote_addr;
+                fastcgi_param  REMOTE_PORT        $remote_port;
+                fastcgi_param  SERVER_ADDR        $server_addr;
+                fastcgi_param  SERVER_PORT        $server_port;
+                fastcgi_param  SERVER_NAME        $server_name;
 
-                                fastcgi_param  REMOTE_ADDR        $remote_addr;
-                                fastcgi_param  REMOTE_PORT        $remote_port;
-                                fastcgi_param  SERVER_ADDR        $server_addr;
-                                fastcgi_param  SERVER_PORT        $server_port;
-                                fastcgi_param  SERVER_NAME        $server_name;
+                # PHP only, required if PHP was built with --enable-force-cgi-redirect
+                fastcgi_param  REDIRECT_STATUS    200;
+              }
 
-                                # PHP only, required if PHP was built with --enable-force-cgi-redirect
-                                fastcgi_param  REDIRECT_STATUS    200;
-                        }
+              # see https://github.com/SSilence/selfoss/wiki/nginx-configuration
 
-                        # see https://github.com/SSilence/selfoss/wiki/nginx-configuration
-                        #location ~ ^/selfoss/((favicons|thumbnails)/.+)$ {
-                        #       try_files /data/$1 =404;
-                        #}
-                        #location ~ ^/selfoss/$ {
-                                #index index.php;
-                                #try_files index.php =404;
-                                #try_files @selfoss =404;
-                        #}
-                        location ~ ^/selfoss/([^?].+)$ {
-                                #try_files /public/$1 /index.php$is_args$args;
-                                #try_files /selfoss/public/$1 @selfoss;
-                                #try_files /public/$1 /public/$1;
-                                alias /var/lib/selfoss/public/$1;
-                        }
+              # regex matches win so make this a regex match
+              location ~ ^/selfoss/favicons/(.*)   { alias /var/lib/selfoss/data/favicons/$1; }                                                                     
+              location ~ ^/selfoss/thumbnails/(.*) { alias /var/lib/selfoss/data/thumbnails/$1; }                                                                   
+
+              location ~ ^/selfoss/([^?].+)$ {
+                      alias /var/lib/selfoss/public/$1;
+              }
             '';
           };
-          #location @selfoss {
-          #         root /var/lib/selfoss;
-          #                 fastcgi_pass unix:/run/phpfpm/my_selfoss_pool.sock;
-          #                 include /nix/store/ljlr3i0j2lb4xb0m3m3752g8d9xfn645-nginx-1.14.2/conf/fastcgi_params;                                                         
-          #                 include /nix/store/ljlr3i0j2lb4xb0m3m3752g8d9xfn645-nginx-1.14.2/conf/fastcgi.conf;                                                           
-          #         fastcgi_param SCRIPT_FILENAME /var/lib/selfoss/index.php;
-          # }
         };
       };
 
