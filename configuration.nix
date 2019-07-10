e887c795110066600b5ff5aaecc8abea2041f9ff
Your Name <root@nixos>
2019-07-10 03:17:02 +0200

    add forward handy->fhem, make forward handy->gallery more specific

--- configuration.nix
+++ configuration.nix
@@ -200,6 +200,8 @@ in {
     iptables -w -A fw-reject -j REJECT
     iptables -w -A FORWARD -i vpn_android-+ -o tinc.bbbsnowbal -p tcp -d 192.168.84.36 --dport 443 -j ACCEPT  # calendar
     iptables -w -A FORWARD -o vpn_android-+ -i tinc.bbbsnowbal -p tcp -s 192.168.84.36 --sport 443 -j ACCEPT
+    iptables -w -A FORWARD -i vpn_android-+ -o tinc.bbbsnowbal -p tcp -d 192.168.84.47 --dport 80 -j ACCEPT  # fhem
+    iptables -w -A FORWARD -o vpn_android-+ -i tinc.bbbsnowbal -p tcp -s 192.168.84.47 --sport 80 -j ACCEPT
     iptables -w -A FORWARD -i vpn_+ -o ens3 -d 192.168.0.0/16,127.0.0.0/8 -j fw-reject
     iptables -w -A FORWARD -i vpn_+ -o ens3 -j ACCEPT
     iptables -w -A FORWARD -o vpn_+ -i ens3 -j ACCEPT
@@ -213,8 +215,16 @@ in {
     iptables -w -t nat -A PREROUTING -i vpn_android-+ -d 192.168.112.10/32 -p tcp --dport 80 -j DNAT --to-destination ${upstreamIP}:${toString ports.rss.port}
     iptables -w -t nat -A PREROUTING -i vpn_android-+ -d 192.168.118.10/32 -p tcp --dport 80 -j DNAT --to-destination ${upstreamIP}:${toString ports.notes-magpie-ext.port}
     # Dummy port, copied from old VPN on kim: 1743 on public IP of Kim is redirected to 443 on gallery for access to Davical/calendar
-    iptables -w -t nat -A PREROUTING -i vpn_android-+ -d 37.187.106.83/32 -p tcp --dport 1743 -j DNAT --to-destination 192.168.84.36:443
-    iptables -t nat -I POSTROUTING -d 192.168.84.36/32 -o tinc.bbbsnowbal -j MASQUERADE  # adjust source IP so tinc can handle the packets
+    iptables -w -t nat -A PREROUTING -i vpn_android-+ -s 192.168.88.0/23 -d 37.187.106.83/32 -p tcp --dport 1743 -j DNAT --to-destination 192.168.84.36:443
+    iptables -w -t nat -I POSTROUTING -s 192.168.88.0/23 -d 192.168.84.36/32 -o tinc.bbbsnowbal -j MASQUERADE  # adjust source IP so tinc can handle the packets
+
+    #TODO The second rule is required for the first rule to work and we need a route on bbverl:
+    # route add -host 192.168.88.2 gw 192.168.84.37
+    #allow_port_forward(in_iface, "bbbsnowball-dev", "192.168.84.47", :tcp, 80)
+    #dnat_port_forward(in_iface, "192.168.85.47", :tcp, 80, "bbbsnowball-dev", "192.168.84.47", 80)
+    iptables -w -t nat -A PREROUTING -i vpn_android-+ -s 192.168.88.0/23 -d 192.168.85.47/32 -p tcp --dport 80 -j DNAT --to-destination 192.168.84.47:80
+    iptables -w -t nat -I POSTROUTING -s 192.168.88.0/23 -d 192.168.84.47/32 -o tinc.bbbsnowbal -j MASQUERADE  # adjust source IP so tinc can handle the packets
+
     #TODO We shoud properly filter incoming packets from VPN: deny from vpn_+ in INPUT, allow "--icmp-type destination-unreachable", whitelist appropriate ports
     #TODO This should already be rejected in FORWARD but this is not logged and connection times out instead.
     iptables -w -t nat -A PREROUTING -i vpn_+ -d 192.168.0.0/16 -p tcp -j DNAT --to-destination 127.0.0.2:1
