2096b39244b693810363c0ed1279b72226cce42e
Benjamin Koch <bbbsnowball@gmail.com>
2020-04-06 23:20:53 +0200

    add multiplex plugin

---
+++ multiplex-config.js
@@ -0,0 +1,4 @@
+window.multiplex_config = {
+	url: "https://reveal-js-multiplex-ccjbegmaii.now.sh",
+	id: "0f01eb35a1c09d41"
+};
---
+++ multiplex-master-config.js
@@ -0,0 +1,3 @@
+window.multiplex_master_config = {
+	secret: "15862073898128710368"
+};
