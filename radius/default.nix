0ea7046664ca7fbc158eb707e336d2fafd767d58
root <benny@routeromen>
2020-04-17 04:28:24 +0200

    disable debug logging for radius server

--- radius/default.nix
+++ radius/default.nix
@@ -18,5 +18,10 @@ in {
   services.freeradius.configDir = configDir;
   # test: radtest -x username password 127.0.0.1:18120 10 testing123
 
+  # NixOS unstable has debug disabled by default. As we are still on 19.09,
+  # we have to overwrite the start command to disable it.
+  systemd.services.freeradius.serviceConfig.ExecStart
+    = lib.mkForce "${pkgs.freeradius}/bin/radiusd -f -d ${services.freeradius.configDir} -l stdout";
+
   systemd.services.freeradius.serviceConfig.StateDirectory = "freeradius";
 }
