31b39caefa5820007e8f8acbeb89eacb8bee30d5
root <benny@routeromen>
2020-04-20 01:08:35 +0200

    fixup: don't let container create resolv.conf in systemd-verify directory

--- systemd-verify/verify.sh
+++ systemd-verify/verify.sh
@@ -5,6 +5,7 @@ if [ -z "$1" ] ; then
   find -L /etc/systemd/{system,user} -type f \( -name "*.target" -or -name "*.target.d" \) | xargs ls -1 2>/dev/null | xargs systemd-analyze verify --man=0 --generators=0 2>&1|grep -vf "$dir/whitelist"
 else
   x="$(realpath "$1")"
-  systemd-nspawn --quiet --as-pid2 --pipe --directory "$dir" --bind-ro "/nix/store" --bind-ro "$x:/result" --bind-ro "$x/etc:/etc" --bind-ro "$x/sw:/run/current-system/sw" -E "PATH=/run/current-system/sw/bin:/result/systemd" -- /run/current-system/sw/bin/bash /"$(basename "$0")" 2>&1 \
+  #NOTE We are not using --ephemeral because that creates many empty directories under $dir.
+  systemd-nspawn --quiet --as-pid2 --pipe --volatile=overlay --directory "$dir" --bind-ro "/nix/store" --bind-ro "$x:/result" --bind-ro "$x/etc:/etc" --bind-ro "$x/sw:/run/current-system/sw" -E "PATH=/run/current-system/sw/bin:/result/systemd" -- /run/current-system/sw/bin/bash /"$(basename "$0")" 2>&1 \
     | grep -vF '/etc/localtime does not point into /usr/share/zoneinfo/, not updating container timezone.'
 fi
