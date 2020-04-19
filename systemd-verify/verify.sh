afba8c8962446df19bf87b66170f22ecd42c28ae
root <benny@routeromen>
2020-04-20 00:04:51 +0200

    run systemd check in nspawn
    That way, we are using the correct version of systemd and we avoid using
    files of the current system config. We haven't mounted everything in the
    correct place and we don't filter /nix so there is still some potential
    for false positives as well as false negatives.

--- systemd-verify/verify.sh
+++ systemd-verify/verify.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -e
 dir="$(dirname "$0")"
-#find -L "$1"/etc/systemd/{system,user} -type f ! -name "*.conf" ! -name "*@.*" -exec systemd-analyze verify --man=0 --generators=0 {} \+ 2>&1|grep -vf "$dir/whitelist"
-find -L "$1"/etc/systemd/{system,user} -type f \( -name "*.target" -or -name "*.target.d" \) | xargs ls -1 2>/dev/null | xargs systemd-analyze verify --man=0 --generators=0 2>&1|grep -vf "$dir/whitelist"
-#systemd-analyze verify --man=0 --generators=0 \
-#  $1/etc/systemd/system/*.target.wants/* \
-#  $1/etc/systemd/system/*.target \
-#  $1/etc/systemd/user/*.target.wants/* \
-#  $1/etc/systemd/user/*.target \
-#  2>&1 | grep -vf "$dir/whitelist"
+if [ -z "$1" ] ; then
+  find -L /etc/systemd/{system,user} -type f \( -name "*.target" -or -name "*.target.d" \) | xargs ls -1 2>/dev/null | xargs systemd-analyze verify --man=0 --generators=0 2>&1|grep -vf "$dir/whitelist"
+else
+  x="$(realpath "$1")"
+  systemd-nspawn --quiet --as-pid2 --pipe --directory "$dir" --bind-ro "/nix/store" --bind-ro "$x:/result" --bind-ro "$x/etc:/etc" --bind-ro "$x/sw:/run/current-system/sw" -E "PATH=/run/current-system/sw/bin:/result/systemd" -- /run/current-system/sw/bin/bash /"$(basename "$0")" 2>&1 \
+    | grep -vF '/etc/localtime does not point into /usr/share/zoneinfo/, not updating container timezone.'
+fi
