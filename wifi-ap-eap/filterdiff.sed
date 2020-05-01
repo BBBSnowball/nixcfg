6e50051077eeb2352814e80a945b903433f751be
root <benny@routeromen>
2020-05-01 05:17:43 +0200

    further filter the diff

---
+++ wifi-ap-eap/filterdiff.sed
@@ -0,0 +1,4 @@
+s_^([-]{3}) (default-config|orig|/nix/store/[^/]*)/_\1 a/_
+s_^([+]{3}) (config|/nix/store/[^/]*)/_\1 b/_
+/^[+-]{3} / s_\t[0-9]{4}-.*__
+s/(diff -Naur --no-dereference ).*/\1.../
--- wifi-ap-eap/mkconfig.sh
+++ wifi-ap-eap/mkconfig.sh
@@ -3,7 +3,7 @@ export PATH=$coreutils/bin:$patch/bin
 cp -r $src $out
 chmod -R u+w $out
 # patch was generated like this:
-# diff -Naur --no-dereference default-config/ config/ >config.patch
+# diff -Naur --no-dereference default-config/ config/ | sed -Ef filterdiff.sed >config.patch
 patch -p1 -d $out <$configPatch
 rm $out/mods-config/files/authorize
 ln -s $secretsDir/users $out/mods-config/files/authorize
