6aa393b0e8c7f3c9ec5370047f6aeb93bdc6fe1a
root <benny@routeromen>
2020-05-02 22:38:42 +0200

    always use $XDG_RUNTIME_DIR for emacs server socket

--- configuration.nix
+++ configuration.nix
@@ -280,13 +280,23 @@ in {
     EDITOR="$VISUAL"
     fi
 
-    alias e="emacsclient --create-frame --tty --alternate-editor=\"\""
-    export EDITOR="emacsclient -t"
+    alias e="emacsclient --create-frame --tty -s $EMACS_SERVER_SOCKET --alternate-editor=\"\""
+    export EDITOR="emacsclient -t -s $EMACS_SERVER_SOCKET"
+
+    # nix-shell is setting TEMPDIR/TEMP/TMPDIR/TMP to $XDG_RUNTIME_DIR. I strongly object
+    # to this but it is quite unlikely that this will be fixed soon.
+    # see https://github.com/NixOS/nix/issues/2957
+    # This causes problems for emacs because it looks for the server socket in /tmp or
+    # /run/user depending on whether we are in nix-shell.
+    #
+    # However, the emacs server socket actually should be in $XDG_RUNTIME_DIR so let's always
+    # put it there. Unfortunately, EMACS_SERVER_FILE is for TCP connections so we have to use
+    # a non-standard variable and pass it to emacsclient - see above.
+    export EMACS_SERVER_SOCKET="$XDG_RUNTIME_DIR/emacs$UID/server"
   '';
 
   services.emacs.enable = true;
   services.emacs.package = pkgs.emacs-nox;
-
 
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
