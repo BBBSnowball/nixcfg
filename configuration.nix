12c0949e9295813fc1f16f9f513acadf263a5998
root <benny@routeromen>
2020-05-02 22:47:38 +0200

    fixup: first set variable, then aliases that use it

--- configuration.nix
+++ configuration.nix
@@ -280,9 +280,6 @@ in {
     EDITOR="$VISUAL"
     fi
 
-    alias e="emacsclient --create-frame --tty -s $EMACS_SERVER_SOCKET --alternate-editor=\"\""
-    export EDITOR="emacsclient -t -s $EMACS_SERVER_SOCKET"
-
     # nix-shell is setting TEMPDIR/TEMP/TMPDIR/TMP to $XDG_RUNTIME_DIR. I strongly object
     # to this but it is quite unlikely that this will be fixed soon.
     # see https://github.com/NixOS/nix/issues/2957
@@ -292,7 +289,10 @@ in {
     # However, the emacs server socket actually should be in $XDG_RUNTIME_DIR so let's always
     # put it there. Unfortunately, EMACS_SERVER_FILE is for TCP connections so we have to use
     # a non-standard variable and pass it to emacsclient - see above.
-    export EMACS_SERVER_SOCKET="$XDG_RUNTIME_DIR/emacs$UID/server"
+    EMACS_SERVER_SOCKET="$XDG_RUNTIME_DIR/emacs$UID/server"
+
+    alias e='emacsclient --create-frame --tty -s $EMACS_SERVER_SOCKET --alternate-editor=\"\"'
+    export EDITOR="emacsclient -t -s $EMACS_SERVER_SOCKET"
   '';
 
   services.emacs.enable = true;
