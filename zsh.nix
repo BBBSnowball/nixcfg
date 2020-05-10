7e6b78442fe3d4daf56091567c9f74804d2d458d
root <benny@routeromen>
2020-05-10 02:55:03 +0200

    more zsh config, e.g. enable powerlevel10k

--- zsh.nix
+++ zsh.nix
@@ -4,14 +4,36 @@ let
 in jens // {
   programs = jens.programs // {
     zsh = jens.programs.zsh // {
+      #promptInit = ''
+      #  export PURE_GIT_PULL=0
+      #'' + jens.programs.zsh.promptInit;
       promptInit = ''
-        export PURE_GIT_PULL=0
-      '' + jens.programs.zsh.promptInit;
+        source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      '';
 
       interactiveShellInit = builtins.replaceStrings ["bindkey 'fd' vi-cmd-mode"] ["bindkey 'jk' vi-cmd-mode"] jens.programs.zsh.interactiveShellInit
         + ''
-          # blub
+          zstyle ':completion:*' matcher-list "" '+m:{a-z}={A-Z}' '+m:{A-Z}={a-z}'
+
+          # use the vi navigation keys in menu completion
+          # see https://unix.stackexchange.com/questions/313093/can-i-navigate-zshs-tab-completion-menu-with-vi-like-hjkl-keys
+          zmodload zsh/complist
+          bindkey -M menuselect '^h' vi-backward-char
+          bindkey -M menuselect '^k' vi-up-line-or-history
+          bindkey -M menuselect '^l' vi-forward-char
+          bindkey -M menuselect '^j' vi-down-line-or-history
+
+          bindkey '^k' up-history
+          bindkey '^j' down-history
+
+          # do again to make it actually work
+          bindkey -v
         '';
+
+      ohMyZsh = {
+        enable = false;
+        #theme = "powerlevel10k";
+      };
     };
   };
 
