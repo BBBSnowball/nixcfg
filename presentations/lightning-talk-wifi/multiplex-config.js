bbd05369b04f5fefe3aabc1a1592462945d4a30f
Benjamin Koch <bbbsnowball@gmail.com>
2020-05-17 00:49:55 +0200

    change multiplex id

--- presentations/lightning-talk-wifi/multiplex-config.js
+++ presentations/lightning-talk-wifi/multiplex-config.js
@@ -1,5 +1,5 @@
 window.multiplex_config = {
 	// generate new id and token here: https://talks.c3pb.de/token
 	url: "https://talks.c3pb.de",
-	id: "4b6a407d3e2a546a"
+	id: "b30f8cb76c84e427"
 };
--- presentations/lightning-talk-wifi/multiplex-master-config.js
+++
@@ -1,3 +0,0 @@
-window.multiplex_master_config = {
-	secret: "15862207829689543281"
-};
