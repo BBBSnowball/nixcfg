a3612cd606ddb23cccf3070b7ecc46d4bef850c9
root <benny@routeromen>
2020-05-26 05:07:39 +0200

    run $stdenv/setup when loading the environment

--- direnv/blub2.nix
+++ direnv/blub2.nix
@@ -25,21 +25,21 @@ let
     # see https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/mkshell/default.nix
     phases = ["exportVarsPhase"];
     _DIRENV_OLD_phases = d.phases or [];
-    #exportVarsPhase = "${pkgs.direnv}/bin/direnv export bash >$out";
     exportVarsPhase = ''
-      #FIXME make this optional
-      export IN_NIX_SHELL=impure
-      [ -n "$stdenv" -a -f $stdenv/setup ] && . $stdenv/setup
-
       # We don't want some of the variables.
       # see https://github.com/target/lorri/blob/1aedb78b59df55c33aa6fcf5675360a2ee4e594c/src/ops/direnv/envrc.bash#L126
       unset HOME USER LOGNAME DISPLAY TERM IN_NIX_SHELL TZ PAGER NIX_BUILD_SHELL SHLVL TEMPDIR TMPDIR TEMP TMP NIX_ENFORCE_PURITY OLDPWD PWD SHELL
       unset NIX_LOG_FD
       export phases="$_DIRENV_OLD_phases"
       unset exportVarsPhase nobuildPhase _DIRENV_OLD_phases
-
-      export >$out
+      ( unset PATH out; export ) >$out
     '';
   });
-in exportEnv (callIfFunction (importScopedWithWatch {} ./shell.nix))
+  makeEnvrc = d: pkgs.writeShellScript "envrc-${d.name or "shell"}" ''
+    . ${exportEnv d}
+    export IN_NIX_SHELL=impure
+    export noDumpEnvVars=1
+    [ -n "$stdenv" -a -f $stdenv/setup ] && . $stdenv/setup
+  '';
+in makeEnvrc (callIfFunction (importScopedWithWatch {} ./shell.nix))
 
