0b46b3235484c351137cc5173f8954cb42c6b6cd
Your Name <root@nixos>
2020-05-28 00:58:31 +0200

    remove key for thinknat

--- configuration.nix
+++ configuration.nix
@@ -339,7 +339,7 @@ in {
 
   services.tinc.networks.bbbsnowball = {
     name = "sonline";
-    hosts = {<redacted>, +thinknat};
+    hosts = {<redacted>};
     listenAddress = upstreamIP;
     package = pkgs.tinc;  # the other nodes use stable so no need to use the pre-release
     interfaceType = "tap";  # must be consistent across the network
