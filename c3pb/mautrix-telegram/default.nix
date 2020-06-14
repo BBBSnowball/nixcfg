247bc5fe9bdf9bd2060513bbdd33af148a349029
Benjamin Koch <bbbsnowball@gmail.com>
2020-06-14 17:52:20 +0200

    make test instance of mautrix-telegram

--- c3pb/mautrix-telegram/default.nix
+++ c3pb/mautrix-telegram/default.nix
@@ -1,8 +1,15 @@
 { config, pkgs, lib, ... }:
 let
+  test = true;
+  testSuffix = if test then "-test" else "";
+
   pythonWithPkgs = import ./requirements.nix { inherit pkgs; };
   mautrixTelegram = pkgs.callPackage ./mautrix-telegram-pkg.nix { inherit pythonWithPkgs; };
-  configPatches = [/etc/nixos/c3pb/mautrix-telegram/config-public.patch /etc/nixos/secret/mautrix-telegram/config-private.patch];
+  configPatches = [
+    /etc/nixos/c3pb/mautrix-telegram/config-public.patch
+    /etc/nixos/secret/mautrix-telegram/config-private.patch
+    /etc/nixos/secret/mautrix-telegram/config-test.patch
+  ];
   makeConfig = pkgs.writeShellScript "make-mautrix-telegram-config" ''
     cp ${mautrixTelegram}/example-config.yaml config.yaml
     for p in ${toString configPatches} ; do
@@ -10,18 +17,24 @@ let
     done
   '';
 in {
+  #FIXME remove
   environment.etc.blub.text = "${mautrixTelegram}";
 
-  systemd.services.mautrix-telegram = {
+  users.users."matrix-synapse${testSuffix}" = {
+    isNormalUser = false;
+    home = "/var/lib/mautrix-telegram${testSuffix}";
+  };
+
+  systemd.services."mautrix-telegram${testSuffix}" = {
     after = ["network.target" "matrix-synapse.service"];
     description = "Matrix-to-Telegram Bridge";
     serviceConfig = {
       Type = "simple";
       ExecStartPre = "+${pkgs.bash}/bin/bash ${makeConfig}";
-      ExecStart = "/bin/bash -c \"source ./bin/activate && exec python -m mautrix_telegram\"";
-      WorkingDirectory = "/var/lib/mautrix-telegram";
-      StateDirectory = "matrix-telegram";
-      User = "mautrix-telegram";
+      ExecStart = "/bin/bash -c \"source ./bin/activate && exec python -m mautrix_telegram\"";  #FIXME !!!!
+      WorkingDirectory = "/var/lib/mautrix-telegram${testSuffix}";
+      StateDirectory = "matrix-telegram${testSuffix}";
+      User = "mautrix-telegram${testSuffix}";
       Restart = "on-failure";
       RestartSec = 10;
     };
