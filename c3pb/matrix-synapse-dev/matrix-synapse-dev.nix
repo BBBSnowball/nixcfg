21bd11d862d8c88d0d45a0b4f30beba656b697bb
Benjamin Koch <bbbsnowball@gmail.com>
2020-06-30 23:00:02 +0200

    add config for Riot

--- c3pb/matrix-synapse-dev/matrix-synapse-dev.nix
+++ c3pb/matrix-synapse-dev/matrix-synapse-dev.nix
@@ -2,6 +2,21 @@
 let
   srcPath = "/home/test/synapse";
   port = 8030;
+  webclientSrc = pkgs.fetchzip {
+    url = "https://github.com/vector-im/riot-web/releases/download/v1.6.7/riot-v1.6.7.tar.gz";
+    sha256 = "1a5rvqw7n41887b30hxv30v8a5q8pc8p83bqklf86mrq09hb2nn6";
+  };
+  webclientConfig = pkgs.writeText "riot-config.json" (lib.generators.toJSON {} {
+    default_server_config."m.homeserver" = {
+      base_url    = config.services.matrix-synapse.public_baseurl;
+      server_name = config.services.matrix-synapse.server_name;
+    };
+  });
+  webclientWithConfig = pkgs.runCommand "riot" {} ''
+    mkdir $out
+    ln -s ${webclientSrc}/* $out/
+    ln -s ${webclientConfig} $out/config.json
+  '';
 in {
   config = {
     services.matrix-synapse = {
@@ -38,10 +53,7 @@ in {
 
       extraConfig = ''
         suppress_key_server_warning: true
-        web_client_location: ${pkgs.fetchzip {
-          url = "https://github.com/vector-im/riot-web/releases/download/v1.6.7/riot-v1.6.7.tar.gz";
-          sha256 = "1a5rvqw7n41887b30hxv30v8a5q8pc8p83bqklf86mrq09hb2nn6";
-        }}
+        web_client_location: ${webclientWithConfig}
       '';
       extraConfigFiles = [
         "${srcPath}/oidc-config.yaml"
