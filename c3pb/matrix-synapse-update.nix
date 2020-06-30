806ca6a8ac5e3d50816f07fa70ccd5c8714ab9af
Benjamin Koch <bbbsnowball@gmail.com>
2020-06-30 19:06:41 +0200

    clean up overlay for Synapse
    As NixOS has a new enough version, we don't have to change version
    and source. This makes it much shorter.

--- c3pb/matrix-synapse-update.nix
+++ c3pb/matrix-synapse-update.nix
@@ -1,25 +1,5 @@
-self: super: let
-  needsUpdate = super.matrix-synapse.version == "1.13.0";
-  newAuthLib = self.python3.pkgs.authlib.overrideAttrs (old: old // rec {
-    version = "0.14.3";
-    src = self.fetchFromGitHub {
-      owner = "lepture";
-      repo = "authlib";
-      rev = "v${version}";
-      sha256 = "0ph97j94i40jj7nc5ya8pfq0ccx023zbqpcs5hrxmib53g64k5xy";
-    };
-  });
-  newSynapse = super.matrix-synapse.overrideAttrs (old: old // rec {
-    # We need Synapse 1.14.0 for OpenID Connect. This is already available in master:
-    # https://github.com/NixOS/nixpkgs/commit/d11dcafe93208d4de3cb837f18b735db3c343efb
-    version = "1.15.1";
-    name = "${old.pname}-${version}";
-    src = self.python3.pkgs.fetchPypi {
-      inherit (old) pname;
-      inherit version;
-      sha256 = "1hfyqp5bzsqp002synxp9jcm9fwzw9p0gcklmycmv616x3flylkf";
-    };
-    #propagatedBuildInputs = old.propagatedBuildInputs ++ [newAuthLib];
+self: super: {
+  matrix-synapse = super.matrix-synapse.overrideAttrs (old: old // rec {
     patches = [
       ./matrix-synapse-patch-for-gitlab.patch
       ./matrix-synapse-patch-for-existing-users.patch
@@ -27,6 +7,4 @@ self: super: let
     doCheck = false;
     doInstallCheck = false;
   });
-in {
-  matrix-synapse = if needsUpdate || true then newSynapse else super.matrix-synapse;
 }
