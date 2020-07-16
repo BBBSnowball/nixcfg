c420d13e91b1efba67bc2fe781cc00fbabf5d81c
root <benny@routeromen>
2020-07-17 00:00:36 +0200

    combine default.nix and node-env.nix for app and server, add proper reference to source

---
+++ pkgs/default.nix
@@ -0,0 +1,29 @@
+{pkgs ? import <nixpkgs> {
+    inherit system;
+  }, system ? builtins.currentSystem, nodejs ? pkgs."nodejs-13_x"}:
+
+let
+  nodeEnv = import ./node-env.nix {
+    inherit (pkgs) stdenv python2 utillinux runCommand writeTextFile;
+    inherit nodejs;
+    libtool = if pkgs.stdenv.isDarwin then pkgs.darwin.cctools else null;
+  };
+  edumeetSrc = pkgs.fetchFromGitHub {
+    owner = "edumeet";
+    repo  = "edumeet";
+    rev = "3.3";
+    sha256 = "098cs9z90ff8cgp88rqi2x79ma1s1a7w02mzzrix80z61ndinpnf";
+  };
+in
+{
+  app = import ./node-packages-app.nix {
+    inherit (pkgs) fetchurl fetchgit;
+    inherit nodeEnv;
+    inherit edumeetSrc;
+  };
+  server = import ./node-packages-server.nix {
+    inherit (pkgs) fetchurl fetchgit runCommand;
+    inherit nodeEnv;
+    inherit edumeetSrc;
+  };
+}
