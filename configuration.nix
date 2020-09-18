26c43841e99f892bdabfe0e432fbb36792b1ab0f
root <benny@routeromen>
2020-09-18 23:33:48 +0200

    add samba

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,7 @@ in {
       #./sound.nix
       ./smokeping.nix
       ./ntopng.nix
+      ./samba.nix
       ./bbverl/syslog-udp.nix
       ./bbverl/rabbitmq.nix
       ./bbverl/fhem.nix
@@ -342,28 +343,6 @@ in {
   services.lorri.enable = true;
 
   documentation.dev.enable = true;
-
-  services.samba = {
-    enable = true;
-    extraConfig = ''
-      log level = 1 auth:5 winbind:5
-    '';
-    shares = {
-      public = {
-        browsable = "yes";
-        #"guest ok" = "yes";
-        path = "/tmp/samba-printer";
-        "read only" = "no";
-        writable = "yes";
-        "valid users" = "samba-printer";
-      };
-    };
-  };
-  networking.firewall.allowedTCPPorts = [ 139 445 ];
-  users.users.samba-printer = {
-    isNormalUser = true;
-    hashedPassword = "x";
-  };
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
---
+++ samba.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+{
+  environment.systemPackages = [ pkgs.samba ];
+
+  services.samba = {
+    enable = true;
+    extraConfig = ''
+      log level = 1 auth:5 winbind:5
+    '';
+    shares = {
+      scans = {
+        path = "/home/scans";
+        comment = "Target for scans from HP printer";
+        browsable = "yes";
+        "guest ok" = "no";
+        "read only" = "no";
+        writable = "yes";
+        "valid users" = "scans";
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 139 445 ];
+
+  users.users.scans = {
+    isNormalUser = true;
+    createHome = true;
+    #NOTE Samba user must be added with `smbpasswd -a scans`. User shouldn't be able to login in Linux.
+    hashedPassword = "x";
+  };
+}
