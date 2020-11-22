ee5106821db11b4a246426a618b50da460ae2fc5
root <root@gpd>
2020-11-22 04:13:05 +0100

    use port 2 for smsd, again
    - ModemManager was having some trouble with port 2 (though not as
      much as before).
    - ModemManager cannot use port 1 because it doesn't speak AT. I had
      confused this with port 0 during some of my testing.
    - The real problem wasn't the warnings in syslog. ModemManager did
      indeed detect that the pin is required but the message wasn't
      all too clear (cannot enable/disable modem: locked). It didn't
      use the pin of the existing configuration because that was locked
      to another device (I couldn't find any indication of that in the
      GUI, though). It does work with a newly created connection.

--- wwan.nix
+++ wwan.nix
@@ -10,15 +10,14 @@
     # There is usually more than one ttyUSB. You need one for the modem and one for SMS and both of them must respond
     # to AT commands. You can test this by opening the port with picocom or `smsd -C` and sending "AT\n". The reply
     # should be "OK".
-    # For E3531, port 1 is not an AT but port 0 and 2 are. However, ModemManager doesn't like port 0 for some reason so
-    # we are using that for smsd.
+    # For E3531, port 1 is not an AT but port 0 and 2 are.
 
     # I had no luck with ATTRS{bInterfaceNumber} so we are using ENV{ID_USB_INTERFACE_NUM}.
     # see https://stackoverflow.com/questions/19174482/udev-rule-with-binterfacenumber-doesnt-work
 
     # We are telling ModemManager to leave this port alone and we are telling systemd to start smsd. We also create a
     # symlink. This is useful for debugging.
-    ATTRS{idVendor}=="12d1", ATTRS{idProduct}=="1001", ENV{ID_USB_INTERFACE_NUM}=="00", \
+    ATTRS{idVendor}=="12d1", ATTRS{idProduct}=="1001", ENV{ID_USB_INTERFACE_NUM}=="02", \
       ENV{ID_MM_PORT_IGNORE}="1", \
       GROUP="smsd", ENV{SMSD}="1", TAG+="systemd", ENV{SYSTEMD_WANTS}="smsd@", GROUP="smsd", MODE="0660", SYMLINK="ttySMS"
   '';
