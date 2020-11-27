db973cb1f0559c1adf564eea58ce30512c16cd0d
root <benny@routeromen>
2020-11-27 22:26:41 +0100

    improve test script

--- test-for-short-time.sh
+++ test-for-short-time.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-set -e
+# We do *not* abort this script in case of errors because that would abort the recovery.
+#set -e
 
 if [ -z "$1" ] ; then
   echo "Usage: $0 result-symlink" >&2
@@ -10,26 +11,46 @@ elif [ ! -x "$1/bin/switch-to-configuration" ] ; then
   exit 1
 fi
 
-cp --remove-destination -dT /run/current-system result.old
+cp --remove-destination -dT /run/current-system result.old || exit $?
 
-"$1/bin/switch-to-configuration" test || true
+logger "test-for-short-time.sh: switching to configuration $1 (`realpath "$1"`)..."
+timeout 300 "$1/bin/switch-to-configuration" test
 
 echo ""
 echo "========================"
 echo ""
-echo "Configuration has been applied. We will roll back to the old configuration in 30 seconds. Press ctrl+c to keep the current configuration."
+echo "Configuration has been applied. We will roll back to the old configuration soon. Press ctrl+c to keep the current configuration."
 echo ""
 echo "If you want to keep the configuration, don't forget to do: $1/bin/switch-to-configuration boot"
 
-for x in 60 50 40 30 20 10 ; do
+for x in `seq 180 -10 10` ; do
   echo -n "$x..  "
   sleep 10
 done
 echo ""
 
 echo "Rolling back to $(realpath result.old)..."
-if ! timeout 300 "./result.old/bin/switch-to-configuration" test ; then
-  echo "Rollback has failed. Trying reboot."
-  reboot
-fi
+logger "test-for-short-time.sh: rollback to configuration $PWD/result.old (`realpath result.old`)..."
+timeout 300 "./result.old/bin/switch-to-configuration" test
+
+echo "I will reboot in 180 seconds unless you press ctrl+c"
+
+for x in `seq 180 -10 10` ; do
+  echo -n "$x..  "
+  sleep 10
+done
+echo ""
+
+echo "Rebooting..."
+logger "test-for-short-time.sh: rebooting..."
+reboot
+
+sleep 180
+
+echo "Will do hard reboot soon..."
+sleep 10
+logger "test-for-short-time.sh: hard reboot..."
+sleep 1
+sync
+reboot -f
 
