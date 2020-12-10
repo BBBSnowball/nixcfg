19842a1ddfdba1f4511e26d37f2056e68c228bc5
root <benny@routeromen>
2020-12-10 06:11:41 +0100

    fixup

--- fix-sudo.nix
+++ fix-sudo.nix
@@ -8,7 +8,7 @@
     else
       TUSER="root"
     fi
-    shell=$(getent passwd $TUSER 2>/dev/null | { IFS=: read _ _ _ _ _ _ x; echo "$x"; })
+    shell="$(getent passwd "$TUSER" 2>/dev/null | { IFS=: read _ _ _ _ _ _ x; echo "$x"; })"
     exec machinectl shell --setenv=SHELL="$shell" "$TUSER@" "$shell" --login -i
   '') ];
 }
