00d174fbdd378f3e7bd281b0644eff7bcd57043c
Benjamin Koch <bbbsnowball@gmail.com>
2021-01-01 06:03:34 +0100

    allow more (visible) users

--- c3pb/letsmeet/config.app.js
+++ c3pb/letsmeet/config.app.js
@@ -148,11 +148,11 @@ var config =
 	hideTimeout          : 3000,
 	// max number of participant that will be visible in 
 	// as speaker
-	lastN                : 9,
+	lastN                : 20, //9,
 	mobileLastN          : 1,
 	// Highest number of lastN the user can select manually in 
 	// userinteface
-	maxLastN             : 20,
+	maxLastN             : 50, //20,
 	// If truthy, users can NOT change number of speakers visible
 	lockLastN            : false,
 	// Show logo if "logo" is not null, else show title
