1eaa1d3f1a477a139ff1491fd65626ad1ff84775
root <benny@routeromen>
2021-01-14 20:45:17 +0100

    add current nixpkgs to NIX_PATH (in addition to flake registry)

--- bootstrap/rebuild-nonflake.sh
+++ bootstrap/rebuild-nonflake.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-exec nix-build '<nixpkgs/nixos>' -A system "$@"
+# We use realpath on nixpkgs to avoid building a derivation that makes /etc/current-nixpkgs point to itself.
+exec nix-build '<nixpkgs/nixos>' -A system "$@" -I nixpkgs="$(realpath "$(nix-instantiate --eval -E "<nixpkgs>")")"
--- modules/enable-flakes.nix
+++ modules/enable-flakes.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }@args:
+{ config, pkgs, lib, ... }@args:
 with lib;
 {
   # see https://www.tweag.io/blog/2020-07-31-nixos-flakes/
@@ -20,9 +20,29 @@ with lib;
     '';
   };
 
-  config.nix.registry.nixpkgs = if args ? nixpkgs
+  options.addCurrentNixpkgsToNixPath = lib.mkOption {
+    type = types.bool;
+    default = true;
+    example = false;
+    description = ''
+      Change NIX_PATH to include the current nixpkgs for <nixpkgs.
+
+      That way, any tool or derivation that uses <nixpkgs> will use the same version of nixpkgs
+      as the current system and thereby they are likely to use versions of packages
+      that are already available in the local nix store.
+    '';
+  };
+
+  config.nix.registry.nixpkgs = lib.mkIf config.addCurrentNixpkgsToRegistry (if args ? nixpkgs
     then { flake = args.nixpkgs; }
-    else { to.type = "path"; to.path = toString <nixpkgs>; };
+    else { to.type = "path"; to.path = toString <nixpkgs>; });
 
   config.environment.etc.current-nixpkgs.source = args.nixpkgs or <nixpkgs>;
+
+  config.nix.nixPath = lib.mkIf config.addCurrentNixpkgsToNixPath [
+    "nixpkgs=/etc/current-nixpkgs"
+    # keep the other default values
+    "nixos-config=/etc/nixos/configuration.nix"
+    "/nix/var/nix/profiles/per-user/root/channels"
+  ];
 }
