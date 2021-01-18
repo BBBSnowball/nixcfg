df9285492062056d45c79ffa47c5f5d6b2a1d993
root <benny@routeromen>
2021-01-18 22:21:40 +0100

    use an overlay for openocd-nuclei
    That way, nixpkgs will do the cross-compile splicing for us.

--- riscv/compiler.nix
+++ riscv/compiler.nix
@@ -8,10 +8,57 @@ let
       rustc.config = "riscv32imac-unknown-none-elf";
     };
     config.allowUnsupportedSystem = true;
+    overlays = [ overlay ];
+  };
+  openocd-nuclei =
+    { openocd, fetchFromGitHub, tcl, which, gnum4, automake, autoconf, libtool, libusb-compat-0_1 }:
+    openocd.overrideAttrs (old: {
+      pname = "openocd-nuclei";
+      version = "0.10.0-14";
+
+      src = fetchFromGitHub {
+        owner = "riscv-mcu";
+        repo = "riscv-openocd";
+        rev = "nuclei-0.10.0-14"; #"9e6a7a2e5320cdaeeafcc79debedfd216f443f19"
+        sha256 = "sha256-dNEwrsIlxlWgm7mH16XBKoUVB78pNcJ58i+VjY33wXE=";
+        fetchSubmodules = true;
+      };
+
+      # patches in old.patches are already applied to that version
+      patches = [];
+
+      # autotools are required because we are building from git rather than source download; tcl is useful to avoid
+      # bootstrapping when cross-compiling
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ tcl which gnum4 automake autoconf libtool ];
+
+      # I think this should be in buildInputs but that would pull in Python with gdb for risc-v which is totally unnecessary here.
+      #buildInputs = (old.buildInputs or []) ++ [ p1.pkgsBuildBuild.libusb-compat-0_1 ];
+      buildInputs = (old.buildInputs or []) ++ [ libusb-compat-0_1 ];
+
+      preConfigure = ''
+        ./bootstrap nosubmodule
+      '';
+
+      NIX_CFLAGS_COMPILE = old.NIX_CFLAGS_COMPILE + " -Wno-error=maybe-uninitialized -Wno-error=format";
+      configureFlags = old.configureFlags ++ [ "--enable-usbprog" "--enable-rlink" "--enable-armjtagew" ];
+    });
+  overlay = self: super: {
+    openocd-nuclei = self.callPackage openocd-nuclei {};
+
+    # see https://nixos.wiki/wiki/Overlays#Python_Packages_Overlay
+    python3 = super.python3.override {
+      packageOverrides = self2: super2: {
+        cython = super2.cython.overrideAttrs (old: {
+          # We should not depend on GDB for the target because Cython will only be used on the host.
+          buildInputs = [ self.glibcLocales ];
+          depsHostHost = [ self.gdb ];
+        });
+      };
+    };
   };
 in rec {
   pkgs = p;
-  inherit (p.pkgsBuildHost) gcc binutils binutils-unwrapped;
+  inherit (p.pkgsBuildHost) gcc binutils binutils-unwrapped openocd-nuclei;
   gcc-riscv32imac = p1.runCommandLocal "gcc-riscv32imac-${gcc.version}" { } ''
     mkdir $out $out/bin
     for x in as c++ cc g++ gcc ld ld.bfd ; do
@@ -21,28 +68,6 @@ in rec {
       ln -s ${binutils-unwrapped}/bin/riscv32-none-elf-$x $out/bin/riscv32imac-unknown-none-elf-$x
     done
   '';
-  openocd-nuclei = p1.pkgsBuildTarget.openocd.overrideAttrs (old: {
-    pname = "openocd-nuclei";
-    version = "0.10.0-14";
-    src = p1.fetchFromGitHub {
-      owner = "riscv-mcu";
-      repo = "riscv-openocd";
-      rev = "nuclei-0.10.0-14"; #"9e6a7a2e5320cdaeeafcc79debedfd216f443f19"
-      sha256 = "sha256-dNEwrsIlxlWgm7mH16XBKoUVB78pNcJ58i+VjY33wXE=";
-      fetchSubmodules = true;
-    };
-    # patches in old.patches are already applied to that version
-    patches = [];
-    # autotools are required because we are building from git rather than source download; tcl is useful to avoid
-    # bootstrapping when cross-compiling
-    nativeBuildInputs = (old.nativeBuildInputs or []) ++ (with p.pkgsBuildHost; [ tcl which gnum4 automake autoconf libtool ]);
-    buildInputs = (old.buildInputs or []) ++ (with p.pkgsBuildBuild; [ libusb-compat-0_1 ]);
-    preConfigure = ''
-      ./bootstrap nosubmodule
-    '';
-    NIX_CFLAGS_COMPILE = old.NIX_CFLAGS_COMPILE + " -Wno-error=maybe-uninitialized -Wno-error=format";
-    configureFlags = old.configureFlags ++ [ "--enable-usbprog" "--enable-rlink" "--enable-armjtagew" ];
-  });
 
   # https://github.com/NixOS/nixpkgs/issues/68804
   #rustc-riscv = p1.pkgsCross.riscv32-embedded.buildPackages.rustc;
@@ -50,7 +75,7 @@ in rec {
   cargo = p1.cargo.override { inherit rustc; };
 
   shell = p.mkShell {
-    depsBuildBuild = with p.pkgsBuildBuild; [ openssl pkg-config gcc rustc ];
+    depsBuildBuild = with p.pkgsBuildBuild; [ openssl pkg-config gcc rustc openocd-nuclei ];
     nativeBuildInputs = with p.pkgsBuildHost; [
       gcc
       #rustc
