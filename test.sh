331e0e8bb0b155c5ef455394f8aa234f70e7cac7
Benjamin Koch <bbbsnowball@gmail.com>
2021-01-24 06:24:51 +0100

    use inputOverrides to path own flake to sub-flake

--- test.sh
+++ test.sh
@@ -5,11 +5,12 @@ nixos-rebuild build && mv -Tf result result-test-2
 nix-build '<nixpkgs/nixos>' -A system "$@" -I nixpkgs="$(realpath "$(nix-instantiate --eval -E "<nixpkgs>")")" -o result-test-3
 for host in rockpro64-snowball ; do
   #nixos-rebuild build --flake .#$host -o result-test-$host-1
-  #nix build --derivation .#nixosConfigurations.$host.config.system.build.toplevel -o result-test-$host-2
+  # nix-instantiate for flakes, see https://github.com/NixOS/nix/issues/3908
+  nix eval --pure-eval .#nixosConfigurations.$host.config.system.build.toplevel.drvPath
   nix-instantiate -E 'with builtins; with (getFlake (toString ./.)).nixosConfigurations; ('"$host"').config.system.build.toplevel' "$@" --add-root result-test-$host-3
   if [ -e hosts/$host/flake.nix ] ; then
     #nixos-rebuild build --flake ./hosts/$host#$host -o result-test-$host-4
-    #nix build --derivation ./hosts/$host#nixosConfigurations.$host.config.system.build.toplevel -o result-test-$host-5
+    nix eval --pure-eval ./hosts/$host#nixosConfigurations.$host.config.system.build.toplevel.drvPath
     nix-instantiate -E 'with builtins; with (getFlake (toString ./hosts/'"$host"')).nixosConfigurations; ('"$host"').config.system.build.toplevel' "$@" --add-root result-test-$host-6
   fi
 done
