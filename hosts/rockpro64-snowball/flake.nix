3981dff09cd333f35b0ba97dd22ca52f59ee4235
Benjamin Koch <bbbsnowball@gmail.com>
2021-01-24 05:20:02 +0100

    try using ../.. to access parent flake
    It does work in none-flake mode but in pure mode we get:
      # nixos-rebuild test
      error: --- RestrictedPathError ---
      access to path '/nix/store/da7n5grglrmprdlbd4yv078158afd3lc-d1kmjxnc60az2zrp7jz7p0g4jpfgk8vw-source/flake.nix' is forbidden in restricted mode
    
    I'm not sure whether this is on purpose or an error but that won't
    work for us.
    
    We can build the flake in the subdir:
      cd hosts/rockpro64-snowball/
      nix build .#nixosConfigurations.rockpro64-snowball.config.system.build.toplevel

--- hosts/rockpro64-snowball/flake.nix
+++ hosts/rockpro64-snowball/flake.nix
@@ -5,7 +5,8 @@
   inputs.rockpro64Config.url = "github:BBBSnowball/nixos-installer-rockpro64";
   inputs.rockpro64Config.inputs.nixpkgs.follows = "nixpkgs";
   #inputs.routeromen.url = "gitlab:snowball/nixos-config-for-routeromen?host=git.c3pb.de";
-  inputs.routeromen.url = "git+ssh://git@git.c3pb.de/snowball/nixos-config-for-routeromen.git";
+  #inputs.routeromen.url = "git+ssh://git@git.c3pb.de/snowball/nixos-config-for-routeromen.git";
+  inputs.routeromen.url = "path:../..";
   inputs.routeromen.inputs.nixpkgs.follows = "nixpkgs";
   inputs.ldap-to-ssh.url = "gitlab:snowball/ldap-to-ssh/a545515d943493bba2be216b58c3ff9b561d3463?host=git.c3pb.de";
   inputs.flake-registry.url = "github:NixOS/flake-registry";
