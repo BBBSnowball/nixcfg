9a52979bc7fec03c611738237b8129215a9f6a6f
Benjamin Koch <bbbsnowball@gmail.com>
2021-01-27 01:49:07 +0100

    auto-patch files that are downloaded by apio

--- embedded/apio.nix
+++ embedded/apio.nix
@@ -1,4 +1,20 @@
-{ fetchFromGitHub, python3Packages }:
+{ fetchFromGitHub, python3Packages, autoPatchelfHook, bash, nix, mkShell, stdenv, coreutils }:
+let
+  newWheel = python3Packages.buildPythonPackage rec {
+    pname = "wheel";
+    version = "0.36.2";
+    src = fetchFromGitHub {
+      owner = "pypa";
+      repo = "wheel";
+      rev = version;
+      sha256 = "sha256-8lK2UvqBIxUYm6IOuT+Jk71wYbEEjvI7typS3749N9g=";
+    };
+
+    pythonImportsCheck = [ "wheel" ];
+
+    pipInstallFlags = [ "--ignore-installed" ];
+  };
+in
 python3Packages.buildPythonPackage rec {
   pname = "apio";
   version = "6.0.0";
@@ -14,13 +30,24 @@ python3Packages.buildPythonPackage rec {
 
   propagatedBuildInputs = with python3Packages; [
     click semantic-version requests pyjwt colorama pyserial newWheel
+    setuptools
   ];
 
   PYTHONPATH = "${newWheel}/lib/python3.8/site-packages";
 
   passthru.wheel = newWheel;
 
-  patchPhase = ''
-    sed -i 's/click>=5,<7/click>=5,<8/' setup.py
+  patches = [ ./apio.patch ];
+
+  inherit autoPatchelfHook bash nix coreutils;
+  autoPatchShell = builtins.unsafeDiscardOutputDependency (mkShell { buildInputs = [ stdenv autoPatchelfHook ]; }).drvPath;
+
+  postPatch = ''
+    substituteInPlace apio/nixos.py.sh --subst-var autoPatchelfHook --subst-var bash --subst-var nix --subst-var autoPatchShell --subst-var coreutils
+  '';
+
+  postInstall = ''
+    chmod +x apio/nixos.py.sh
+    cp apio/nixos.py.sh $out/lib/python*/site-packages/apio/
   '';
 }
