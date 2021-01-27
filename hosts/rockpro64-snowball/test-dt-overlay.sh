784239ab34b5775eba907da70b62f5bbb6fccbfb
Benjamin Koch <bbbsnowball@gmail.com>
2021-01-27 04:12:58 +0100

    use dummy fragments to add missing labels and phandles

--- hosts/rockpro64-snowball/test-dt-overlay.sh
+++ hosts/rockpro64-snowball/test-dt-overlay.sh
@@ -5,3 +5,5 @@ cd "`dirname "$0"`"
 dtc -I dts -O dtb -@ -o dt-overlay--use-fan.dtbo dt-overlay--use-fan.dts
 dtc -I dtb -O dts dt-overlay--use-fan.dtbo -f
 fdtoverlay -o result -i /run/current-system/dtbs/rockchip/rk3399-rockpro64.dtb dt-overlay--use-fan.dtbo
+dtc -I dtb -O dts result -o result.dts
+$EDITOR result.dts
