0e4dd8080d146137fa833658602786cf7760cc32
Benjamin Koch <bbbsnowball@gmail.com>
2021-01-27 04:03:17 +0100

    try adding fan trips and maps to the overlay

---
+++ hosts/rockpro64-snowball/test-dt-overlay.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p dtc
+set -xe
+cd "`dirname "$0"`"
+dtc -I dts -O dtb -@ -o dt-overlay--use-fan.dtbo dt-overlay--use-fan.dts
+dtc -I dtb -O dts dt-overlay--use-fan.dtbo -f
+fdtoverlay -o result -i /run/current-system/dtbs/rockchip/rk3399-rockpro64.dtb dt-overlay--use-fan.dtbo
