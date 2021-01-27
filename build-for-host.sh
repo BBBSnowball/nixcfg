b62f6445bf8a57f51e0044bb0b869ccd6f9577da
Benjamin Koch <bbbsnowball@gmail.com>
2021-01-27 05:34:42 +0100

    add alternative command in build-for-host.sh

--- build-for-host.sh
+++ build-for-host.sh
@@ -6,3 +6,4 @@ fi
 hostname="$1"
 shift
 exec nix-build -E 'with builtins; with (getFlake (toString ./.)).nixosConfigurations; ('"$hostname"').config.system.build.toplevel' "$@"
+#exec nix build .#nixosConfigurations."$hostname".config.system.build.toplevel "$@"
