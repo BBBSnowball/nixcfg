58a0b2166572f4a32a19e2fcc4694288b508595d
Benjamin Koch <bbbsnowball@gmail.com>
2021-04-04 20:21:47 +0200

    auto-update path:... inputs, get nixUnstable and jq via nix-shell

--- build-for-host.sh
+++ build-for-host.sh
@@ -1,9 +1,31 @@
-#!/bin/sh -e
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p jq nixUnstable
+set -e
 if [ -z "$1" ] ; then
   echo "Usage: $0 hostname [nix-build opts]" >&2
   exit 1
 fi
 hostname="$1"
 shift
+
+NIX=(nix --experimental-features 'nix-command flakes')
+
+if nix flake lock --help &>/dev/null ; then
+  update_cmd=lock
+elif nix flake update --help &>/dev/null ; then
+  update_cmd=update
+else
+  update_cmd=
+fi
+if [ -n "$update_cmd" ] ; then
+  jq <flake.lock '.nodes|to_entries|.[]|if .value.locked.type == "path" then .key else null end|select(.)' -r | xargs -n1 -t $NIX flake $update_cmd --update-input
+  if [ -e "hosts/$hostname/flake.lock" ] ; then
+    ( cd "hosts/$hostname" && jq <flake.lock '.nodes|to_entries|.[]|if .value.locked.type == "path" then .key else null end|select(.)' -r | xargs -n1 -t $NIX flake $update_cmd --update-input )
+    fi
+else
+  echo "WARN: Modern nix tooling not available -> not updating path:... in lock file." >&2
+fi
+
+set -x
 #exec nix-build -E 'with builtins; with (getFlake (toString ./.)).nixosConfigurations; ('"$hostname"').config.system.build.toplevel' "$@"
-exec nix --log-format bar-with-logs build .#nixosConfigurations."$hostname".config.system.build.toplevel "$@"
+exec $NIX --log-format bar-with-logs build .#nixosConfigurations."$hostname".config.system.build.toplevel "$@"
