d4c42906828f82e5579655547a18898a5dc1a3f0
Benjamin Koch <bbbsnowball@gmail.com>
2021-04-04 21:16:57 +0200

    add option for building derivation instead of result

--- build-for-host.sh
+++ build-for-host.sh
@@ -8,6 +8,15 @@ fi
 hostname="$1"
 shift
 
+cmd=build
+extraPath=
+if [ "$1" == "--drv" -o "$1" == "--derivation" ] ; then
+  # see https://github.com/NixOS/nix/issues/3908
+  shift
+  cmd=eval
+  extraPath=.drvPath
+fi
+
 NIX=(nix --experimental-features 'nix-command flakes')
 
 if nix flake lock --help &>/dev/null ; then
@@ -28,4 +37,4 @@ fi
 
 set -x
 #exec nix-build -E 'with builtins; with (getFlake (toString ./.)).nixosConfigurations; ('"$hostname"').config.system.build.toplevel' "$@"
-exec $NIX --log-format bar-with-logs build .#nixosConfigurations."$hostname".config.system.build.toplevel "$@"
+exec $NIX --log-format bar-with-logs $cmd .#nixosConfigurations."$hostname".config.system.build.toplevel$extraPath "$@"
