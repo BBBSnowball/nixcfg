c2757777149465dd42df2ef6ae690a937d9d6dfa
Benjamin Koch <bbbsnowball@gmail.com>
2021-06-26 19:34:51 +0200

    fix bluetooth
    VID and PID are zero so we have to tell the kernel to use the device anyway.

--- bluetooth.nix
+++ bluetooth.nix
@@ -3,4 +3,9 @@
   hardware.bluetooth.enable = true;
   boot.kernelModules = [ "btusb" ];
   hardware.pulseaudio.package = pkgs.pulseaudioFull;
+
+  services.udev.extraRules = ''
+    # http://rolandtanglao.com/2017/09/05/p1-how-to-make-bluetooth-work-gpd-pocket-ubuntu/
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0000", ATTRS{idProduct}=="0000", RUN+="${pkgs.bash}/bin/bash -c 'modprobe btusb; echo 0000 0000 > /sys/bus/usb/drivers/btusb/new_id'"
+  '';
 }
