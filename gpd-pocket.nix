a30719981213dc5d7a3e1cf0d3351e71dd2c15b1
root <root@gpd>
2021-06-27 18:40:58 +0200

    move governor config to gpd submodule

--- gpd-pocket.nix
+++ gpd-pocket.nix
@@ -17,14 +17,6 @@
   services.thermald.enable = true;
   services.tlp.enable = true;
 
-  # hardware-configuration.nix sets a default value of powersave but that's a bad idea
-  # when not using the intel_pstate driver. As we have enabled tlp, this will be set
-  # via tlp instead of a dedicated cpufreq service.
-  # ARK for Z8750 doesn't even mention Speed Shift Technology so I guess it is correct that intel_pstate isn't used here.
-  # https://ark.intel.com/content/www/us/en/ark/products/93362/intel-atom-x7-z8750-processor-2m-cache-up-to-2-56-ghz.html
-  #powerManagement.cpuFreqGovernor = "schedutil";
-  powerManagement.cpuFreqGovernor = null;  # keep scheduler that is selected by the kernel
-
   # https://nixos.wiki/wiki/Accelerated_Video_Playback
   #nixpkgs.config.packageOverrides = pkgs: {
   #  vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
