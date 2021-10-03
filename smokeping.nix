3af813e58f8e4db5301a2008f790d24f78b06841
Benjamin Koch <bbbsnowball@gmail.com>
2021-10-03 23:47:44 +0200

    add speedtest probes for smokeping

--- smokeping.nix
+++ smokeping.nix
@@ -69,12 +69,31 @@ let
   '';
   targetHostsText = lib.concatMapStringsSep "" renderProbe targetHostsPerProbe;
   # compare to previous: nixos-rebuild build && ( x="$(sed -En 's/^ExecStart=([^ ]*)( .*)?/\1/p' result/etc/systemd/system/smokeping.service)"; y="$(sed -En 's/.*--config=([^ ]*) .*/\1/p' "$x")"; diff /nix/store/mck2kz613hcs8d35mf0p0bm9pg8jpd2q-smokeping.conf "$y" -u )
+
+  speedtestPlugin = pkgs.fetchFromGitHub {
+    owner = "mad-ady";
+    repo  = "smokeping-speedtest";
+    rev   = "c0d4a60cc7eb8b18c5879797cbf0be81e18ce790";  # 2020-05-22
+    sha256 = "sha256-ribrGZgVDW5/aXY8obs9+ZRczWYZlC8vZ9FZvcY7zus=";
+  };
+  # speedtest values can be higher than 180
+  smokepingLargeValuesPatch = pkgs.fetchurl {
+    url = "https://github.com/oetiker/SmokePing/commit/60419834f224a0735094fd4ad0aac8eac3b15289.patch";
+    sha256 = "sha256-2Bu5RHkigLx7uE2F1GScawssg6sY+LDeOUxqtM4n8kc=";
+  };
 in
 {
   nixpkgs.overlays = [
     (self: super: {
       smokeping = super.smokeping.overrideAttrs (old: {
-        patches = (old.patches or []) ++ [./smokeping-drop-rsa1.patch];
+        patches = (old.patches or []) ++ [
+          ./smokeping-drop-rsa1.patch
+          smokepingLargeValuesPatch
+        ];
+        postInstall = (old.postInstall or "") + ''
+          # add speedtest plugin
+          cp ${speedtestPlugin}/*.pm $out/lib/Smokeping/probes/
+        '';
       });
     })
   ];
@@ -108,6 +127,20 @@ in
       + SSH
       binary = ${pkgs.openssh}/bin/ssh-keyscan
       offset = 60%
+
+      + speedtest
+      binary = ${pkgs.speedtest-cli}/bin/speedtest-cli
+      timeout = 300
+      forks = 1
+      step = 3600
+      offset = random
+      pings = 3
+      ++ speedtest-download
+      measurement = download
+      #extraargs = --no-upload
+      ++ speedtest-upload
+      measurement = upload
+      #extraargs = --no-download
     '';
     targetConfig = ''
       probe = FPingNormal
@@ -159,6 +192,32 @@ in
       title = ssh-keyscan bkoch.eu
       host = bkoch.eu
       port = 22761
+
+      + Speedtest
+      menu = Speedtest
+      title = Speedtest
+      ++ from_closest
+      title = from closest
+      menu = from closest
+      probe = speedtest-download
+      host = dummy.com
+      ++ to_closest
+      title = to closest
+      menu = to closest
+      probe = speedtest-upload
+      host = dummy.com
+      ++ from_frace1
+      title = from France
+      menu = from France
+      probe = speedtest-download
+      server = 39765
+      host = dummy.com
+      ++ to_france1
+      title = to France
+      menu = to France
+      probe = speedtest-upload
+      server = 39765
+      host = dummy.com
     '';
   };
 }
