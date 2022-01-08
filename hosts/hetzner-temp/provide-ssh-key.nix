5c78928d98ad251eeb0018a5734c2a921c4d7487
Benjamin Koch <bbbsnowball@gmail.com>
2022-01-08 06:21:11 +0100

    more fixups

--- hosts/hetzner-temp/provide-ssh-key.nix
+++ hosts/hetzner-temp/provide-ssh-key.nix
@@ -7,15 +7,18 @@
     serviceConfig.PermissionsStartOnly = true;
     serviceConfig.ProtectSystem = "full";
 
+    after = [ "sshd.service" ];
+    wantedBy = [ "multi-user.target" ];
+
     path = with pkgs; [ openssl socat gnutar ];
 
     preStart = ''
-      umask 077
       cd $RUNTIME_DIRECTORY
       cat /etc/ssh/ssh_host_*_key.pub >host-keys
       openssl dgst -sha256 -hmac <(cat /etc/ssh-shared-secret) -binary <host-keys | openssl enc -base64 -A >host-keys.sig
       tar -cf keys.tar host-keys host-keys.sig
       rm host-keys{,.sig}
+      chmod 644 keys.tar
     '';
     script = ''
       cd $RUNTIME_DIRECTORY
