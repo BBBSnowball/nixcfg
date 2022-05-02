376172c179d39beb25d5da5d01f4ba14789e2841
Benjamin Koch <bbbsnowball@gmail.com>
2022-05-03 01:37:44 +0200

    use systemd LoadCredential to pass config to ddclient
    The previous method didn't work anymore because systemd didn't create
    the dynamic user early enough or with the right name.

--- bbverl/ddclient.nix
+++ bbverl/ddclient.nix
@@ -5,6 +5,8 @@
     configFile = "/etc/nixos/secret/ddclient.conf";
   };
 
-  systemd.services.ddclient.serviceConfig.User = "ddclient-dynamic";
-  systemd.services.ddclient.serviceConfig.ExecStartPre = lib.mkForce "+${lib.getBin pkgs.coreutils}/bin/install -o ddclient-dynamic -m400 ${config.services.ddclient.configFile} /run/ddclient/ddclient.conf";
+  # The NixOS service uses ExecStartPre (or preStart) to install the config to the services run directory. We do the same but make sure
+  # that it is only readable for the service. This may be a bit too paranoid.
+  systemd.services.ddclient.serviceConfig.LoadCredential = "config:/etc/nixos/secret/ddclient.conf";
+  systemd.services.ddclient.serviceConfig.ExecStartPre = lib.mkForce ''${lib.getBin pkgs.bash}/bin/bash -c "${lib.getBin pkgs.coreutils}/bin/ln -s $CREDENTIALS_DIRECTORY/config /run/ddclient/ddclient.conf"'';
 }
