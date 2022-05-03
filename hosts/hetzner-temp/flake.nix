73d859609daab90e1f03a0113bab730b76cf6dbd
Benjamin Koch <bbbsnowball@gmail.com>
2022-05-03 03:18:27 +0200

    update host hetzner-temp

--- hosts/hetzner-temp/flake.nix
+++ hosts/hetzner-temp/flake.nix
@@ -5,13 +5,6 @@
   inputs.flake-registry.url = "github:NixOS/flake-registry";
   inputs.flake-registry.flake = false;
 
-  inputs.dummy.url = "github:BBBSnowball/nixcfg/dummy";
-  inputs.routeromen.inputs.private.follows = "dummy";
-  inputs.routeromen.inputs.private-nixosvm.follows = "dummy";
-  inputs.routeromen.inputs.private-c3pbvm.follows = "dummy";
-  inputs.routeromen.inputs.private-gk3v-pb.follows = "dummy";
-  inputs.private.follows = "dummy";
-
-  outputs = { self, nixpkgs, routeromen, private, ... }@flakeInputs:
+  outputs = { self, nixpkgs, routeromen, ... }@flakeInputs:
     routeromen.lib.mkFlakeForHostConfig "hetzner-temp" "x86_64-linux" ./main.nix flakeInputs;
 }
