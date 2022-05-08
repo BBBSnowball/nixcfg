6ae8d25b64bcd1e4e5342c161d9ef7b01a9146c3
root <root@gk3v-pb>
2022-05-08 04:42:20 +0200

    gk3v: add tinc network "a"

---
+++ modules/tinc-client-a.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ... }:
+let
+  name       = "a";
+  private    = "/etc/nixos/private/data";
+  tincIP     = (builtins.head config.networking.interfaces."tinc.${name}".ipv4.addresses).address;
+in
+{
+  environment.systemPackages = [ pkgs.tinc ];
+
+  services.tinc.networks.${name} = {
+    name = builtins.replaceStrings ["-"] ["_"] config.networking.hostName;
+    package = pkgs.tinc;  # the other nodes use stable so no need to use the pre-release
+    interfaceType = "tap";  # must be consistent across the network
+    chroot = true;
+    extraConfig = ''
+      AddressFamily=ipv4
+      Mode=switch
+      LocalDiscovery=yes
+      ConnectTo=sonline
+      #ConnectTo=routeromen
+
+      Port 657
+    '';
+  };
+
+  # mkdir /etc/nixos/private/data/tinc-pubkeys/bbbsnowball -p
+  # nix-shell -p tinc --run "tincd -K 4096 -n bbbsnowball"
+  #   /etc/nixos/secret/tinc-bbbsnowball-rsa_key.priv
+  #   /etc/nixos/private/data/tinc-pubkeys/bbbsnowball/`hostname`
+  # scp -3 gk3v:/etc/nixos/private/data/tinc-pubkeys/bbbsnowball/`hostname` omen-verl-remote:/etc/nixos/private/data/tinc-pubkeys/bbbsnowball/`hostname`
+  # scp -3 gk3v:/etc/nixos/private/data/tinc-pubkeys/bbbsnowball/`hostname` nixos:/etc/nixos/private/tinc/pubkeys/bbbsnowball/`hostname`
+  # scp -3 "root@nixos:/etc/nixos/private/tinc/pubkeys/bbbsnowball/*" root@`hostname`:/etc/nixos/private/data/tinc-pubkeys/bbbsnowball/
+
+  systemd.services."tinc.${name}".preStart = ''
+    ${pkgs.coreutils}/bin/install -o tinc.${name} -m755 -d /etc/tinc/${name}/hosts
+    ${pkgs.coreutils}/bin/install -o root -m400 /etc/nixos/secret/tinc-${name}-rsa_key.priv /etc/tinc/${name}/rsa_key.priv
+
+    #${pkgs.coreutils}/bin/install -o tinc.${name} -m444 ${private}/tinc-pubkeys/${name}/* /etc/tinc/${name}/hosts/
+    ${pkgs.rsync}/bin/rsync -r --delete ${private}/tinc-pubkeys/${name}/ /etc/tinc/${name}/hosts
+    chmod 444 /etc/tinc/${name}/hosts/*
+    chown -R tinc.${name} /etc/tinc/${name}/hosts/
+  '';
+}
