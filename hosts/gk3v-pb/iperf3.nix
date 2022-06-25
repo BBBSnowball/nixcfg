135d4acdbe8322316021353a9a51d8f5fd5ed550
root <root@gk3v-pb>
2022-06-25 17:39:27 +0200

    add info on how to run iperf3 on Omada EP115 access point

--- hosts/gk3v-pb/iperf3.nix
+++ hosts/gk3v-pb/iperf3.nix
@@ -45,4 +45,11 @@
   networking.firewall.allowedUDPPorts = [
     config.services.iperf3.port
   ];
+
+  # static build for mips (for running on old Omada APs):
+  # ##with openssl, huge: nix-build -E '(import <nixpkgs> { crossSystem = { config = "mips-unknown-linux-gnu"; }; config.allowUnsupportedSystem = true; }).pkgsStatic.iperf3'
+  # nix-build -E '((import <nixpkgs> { crossSystem = { config = "mips-unknown-linux-gnu"; }; config.allowUnsupportedSystem = true; }).pkgsStatic.iperf3.override { openssl = null; }).overrideAttrs (old: { configureFlags = []; })'
+  # ssh user@ap 'cat >/tmp/logdump/iperf3' <result/bin/iperf3
+  # ssh user@ ap 'cd /tmp/logdump && TEMP=. TMPDIR=. ./iperf3 -c target'  # tested on TP-Link EAP115
+  # (server mode doesn't work unless we find an open port in the firewall)
 }
