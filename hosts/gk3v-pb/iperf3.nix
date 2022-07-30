d91c3fb153b6c4710a4203c27e4f70bdac00db18
root <root@gk3v-pb>
2022-07-30 16:55:10 +0200

    gk3v: fix UDP mode for iperf3

--- hosts/gk3v-pb/iperf3.nix
+++ hosts/gk3v-pb/iperf3.nix
@@ -4,44 +4,9 @@
     enable = true;
     openFirewall = true;
   };
-  #systemd.services.iperf3-udp = let tcp = config.systemd.services.iperf3; in tcp // {
-  #  serviceConfig = tcp.serviceConfig // {
-  #    ExecStart = tcp.serviceConfig.ExecStart + " --udp";
-  #  };
-  #};
-  #networking.firewall.allowedUDPPorts = [ config.systemd.services.iperf3.port ];
 
-  # copied from https://github.com/NixOS/nixpkgs/blob/nixos-22.05/nixos/modules/services/networking/iperf3.nix  
-  systemd.services.iperf3-udp = let cfg = config.services.iperf3; in with lib; {
-    description = "iperf3 daemon";
-    unitConfig.Documentation = "man:iperf3(1) https://iperf.fr/iperf-doc.php";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
-
-    serviceConfig = {
-      Restart = "on-failure";
-      RestartSec = 2;
-      DynamicUser = true;
-      PrivateDevices = true;
-      CapabilityBoundingSet = "";
-      NoNewPrivileges = true;
-      ExecStart = ''
-        ${pkgs.iperf3}/bin/iperf \
-          --server \
-          --port ${toString cfg.port} \
-          ${optionalString (cfg.affinity != null) "--affinity ${toString cfg.affinity}"} \
-          ${optionalString (cfg.bind != null) "--bind ${cfg.bind}"} \
-          ${optionalString (cfg.rsaPrivateKey != null) "--rsa-private-key-path ${cfg.rsaPrivateKey}"} \
-          ${optionalString (cfg.authorizedUsersFile != null) "--authorized-users-path ${cfg.authorizedUsersFile}"} \
-          ${optionalString cfg.verbose "--verbose"} \
-          ${optionalString cfg.debug "--debug"} \
-          ${optionalString cfg.forceFlush "--forceflush"} \
-          ${escapeShellArgs cfg.extraFlags} \
-          --udp
-      '';
-    };
-  };
-
+  # UDP test is selected by the client so we don't need a separate server for that
+  # but we do have to open the port.
   networking.firewall.allowedUDPPorts = [
     config.services.iperf3.port
   ];
