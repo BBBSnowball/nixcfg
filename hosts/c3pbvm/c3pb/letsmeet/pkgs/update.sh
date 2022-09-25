021fa4ca0fe8c64817463b5178b7826a4a2303f5
Benjamin Koch <bbbsnowball@gmail.com>
2022-09-25 06:48:44 +0200

    fix update scripts

--- hosts/c3pbvm/c3pb/letsmeet/pkgs/update.sh
+++ hosts/c3pbvm/c3pb/letsmeet/pkgs/update.sh
@@ -4,8 +4,10 @@ mkdir -p tmp
 chown test tmp
 #systemd-run --uid test --pty --same-dir --wait --collect --service-type=exec
 nixpkgs=$(nix eval --impure --expr 'with builtins; let x = getFlake (toString ../../..); in x.inputs.nixpkgs.outPath' --raw)
+#machinectl shell test@ \
+#  `which nix-shell` -p "(nodePackages.override { nodejs = (import $PWD/default.nix {}).nodejs; }).node2nix" -I nixpkgs=$nixpkgs --run "$PWD/update2.sh $PWD"
 machinectl shell test@ \
-  `which nix-shell` -p "(nodePackages.override { nodejs = (import $PWD/default.nix {}).nodejs; }).node2nix" -I nixpkgs=$nixpkgs --run "$PWD/update2.sh $PWD"
+  `which nix-shell` -p "nodePackages.node2nix" -I nixpkgs=$nixpkgs --run "$PWD/update2.sh $PWD"
 
 diff tmp/src/app/node-env.nix tmp/src/server/node-env.nix
 cp tmp/src/app/node-packages.nix    node-packages-app.nix
--- hosts/c3pbvm/c3pb/letsmeet/pkgs/update2.sh
+++ hosts/c3pbvm/c3pb/letsmeet/pkgs/update2.sh
@@ -7,7 +7,7 @@ rm -f tmp/{app,server}/*.nix
 
 cd tmp
 nix-build ../default.nix -A src -o src-link -I nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos
-cp -Lr src-link src
+cp -LrT src-link src
 chmod -R u+rw .
 
 cd src/app
