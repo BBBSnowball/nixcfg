841114b2da36ab64c26a8d46245453783ae79caf
Benjamin Koch <bbbsnowball@gmail.com>
2022-11-28 01:40:51 +0100

    fw: try virtlyst webinterface for libvirtd/virt-manager

--- hosts/framework/virtmanager.nix
+++ hosts/framework/virtmanager.nix
@@ -1,5 +1,15 @@
+{ pkgs, ... }:
 {
   virtualisation.libvirtd.enable = true;
   users.users.user.extraGroups = [ "libvirtd" ];
+
+  services.virtlyst.enable = true;
+  services.virtlyst.adminPassword = "password";  # Well....
+  # SSH doesn't seem to be tested - at least for the Nix package.
+  # In addition, I had to create ~virtlyst/.ssh/{id_rsa,config,known_hosts}
+  # with appropriate contents. I have set the user in the SSH config because
+  # the user field on the web interface seems to be ignored (but I probably
+  # could have added it to the hostname).
+  systemd.services.virtlyst.path = [ pkgs.openssh ];
 }
 
