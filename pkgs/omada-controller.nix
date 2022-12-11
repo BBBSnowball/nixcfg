8371db5c5a4ca8f3354df9d3e094837e20afc324
root <root@gk3v-pb>
2022-12-11 03:53:21 +0100

    omada-controller: 5.6.3 -> 5.7.4

--- pkgs/omada-controller.nix
+++ pkgs/omada-controller.nix
@@ -2,12 +2,12 @@
 { stdenv, fetchzip, openjdk, mongodb }:
 stdenv.mkDerivation {
   pname = "omada-controller";
-  version = "5.6.3";
+  version = "5.7.4";
 
   src = fetchzip {
     # https://www.tp-link.com/de/support/download/omada-software-controller/
-    url = "https://static.tp-link.com/upload/software/2022/202210/20221024/Omada_SDN_Controller_v5.6.3_Linux_x64.tar.gz";
-    hash = "sha256-BONtjubWElXJ2rrxSBrzh+XF5dLJtOCNfO5KGbzNH7s=";
+    url = "https://static.tp-link.com/upload/software/2022/202211/20221121/Omada_SDN_Controller_v5.7.4_Linux_x64.tar.gz";
+    hash = "sha256-SiBy2WxBvGi/KsXL7/yBa4MWb6rsqjOT1sGvWylUbHE=";
   };
 
   buildInputs = [ openjdk mongodb ];
