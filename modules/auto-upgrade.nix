7a32119ad996c5e8a1abebb4b9f4fa4be26ab87f
Your Name <root@nixos>
2023-01-20 04:30:15 +0100

    add flake arguments for NixOS autoUpgrade service

--- modules/auto-upgrade.nix
+++ modules/auto-upgrade.nix
@@ -1,6 +1,22 @@
+{ config, lib, ... }:
+let
+  hostName = config.networking.hostName;
+in
 {
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = true;
+
+  system.autoUpgrade.flake = "/etc/nixos/flake#${hostName}";
+  #system.autoUpgrade.flake = "/etc/nixos/flake/hosts/${hostName}#${hostName}";  # doesn't have the `private` input
+  system.autoUpgrade.flags = [
+    "--override-input" "private" "path:/etc/nixos/hosts/${hostName}/private"
+    # This would update the outer flake, which is not what we want:
+    #"--update-input" "nixpkgs" "--commit-lock-file"
+  ];
+
+  systemd.services.nixos-upgrade.script = lib.mkBefore ''
+    nix flake lock /etc/nixos/flake/hosts/${hostName} --update-input nixpkgs --commit-lock-file
+  '';
 
   # https://github.com/NixOS/nixpkgs/issues/79109
   nix.gc.automatic = true;
