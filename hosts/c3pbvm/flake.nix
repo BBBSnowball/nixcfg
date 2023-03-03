98cd80d49e85000ad7c2ecede4096d4a98f9ad29
Benjamin Koch <bbbsnowball@gmail.com>
2023-03-03 07:12:28 +0100

    c3pbvm: update to 22.11

--- hosts/c3pbvm/flake.nix
+++ hosts/c3pbvm/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   inputs.flake-compat.follows = "routeromen/flake-compat";
   inputs.routeromen.url = "github:BBBSnowball/nixcfg";
   inputs.routeromen.inputs.nixpkgs.follows = "nixpkgs";
