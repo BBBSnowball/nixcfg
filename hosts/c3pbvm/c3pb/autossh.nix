e237da9f6e4cace1a5981e260a04c1d5f9cecd0f
Benjamin Koch <bbbsnowball@gmail.com>
2023-03-18 00:33:47 +0100

    c3pbvm: use LoadCredential for autossh

--- hosts/c3pbvm/c3pb/autossh.nix
+++ hosts/c3pbvm/c3pb/autossh.nix
@@ -1,22 +1,39 @@
-{ pkgs, config, lib, privateForHost, ... }:
+{ pkgs, config, lib, privateForHost, secretForHost, ... }:
 {
   services.autossh.sessions = [
     {
       name = "amqp";
       user = "autossh";
-      extraArguments = "-o BatchMode=yes -o ServerAliveInterval=60 -o ServerAliveCountMax=3 ${lib.fileContents "${privateForHost}/autossh-target-hackerspace.txt"} -NL 5672:amqp:5672";
+      monitoringPort = 29485;
+      extraArguments = "-o BatchMode=yes -o ServerAliveInterval=60 -o ServerAliveCountMax=3 hackerspace -NL 5672:amqp:5672";
     }
     #{
     #  name = "ldap";
     #  user = "autossh";
-    #  extraArguments = "-o BatchMode=yes -o ServerAliveInterval=60 -o ServerAliveCountMax=3 ${lib.fileContents "${privateForHost}/autossh-target-ldap.txt"} -NL 3890:localhost:389 -L 6360:localhost:636";
+    #  monitoringPort = 37512;
+    #  extraArguments = "-o BatchMode=yes -o ServerAliveInterval=60 -o ServerAliveCountMax=3 ldap-tunnel -NL 3890:localhost:389 -L 6360:localhost:636";
     #}
   ];
-  systemd.services.autossh-amqp.serviceConfig.Restart = lib.mkForce "always";
-  systemd.services.autossh-amqp.serviceConfig.RestartSec = 10;
-  systemd.services.autossh-ldap.serviceConfig.Restart = lib.mkForce "always";
-  systemd.services.autossh-ldap.serviceConfig.RestartSec = 10;
+  systemd.services = let
+    restartConfig = {
+      Restart = lib.mkForce "always";
+      RestartSec = 10;
+    };
+  in {
+    autossh-amqp = {
+      serviceConfig = restartConfig // {
+        LoadCredential = "id_edi:${secretForHost}/autossh/id_edi";
+        RuntimeDirectory = "autossh";
+        WorkingDirectory = "/run/autossh";
+      };
+      preStart = ''
+        ln -sf ''${CREDENTIALS_DIRECTORY}/id_edi /run/autossh/
+      '';
+    };
+    autossh-ldap.serviceConfig = restartConfig;
+  };
 
+  # The ssh config refers to the IdentityFile in /etc/nixos/secret (for use by root) as well as /run/autossh (for use by the service).
   programs.ssh.extraConfig = lib.fileContents "${privateForHost}/autossh-ssh-config";
   programs.ssh.knownHosts = import "${privateForHost}/autossh-knownhosts.nix";
 
