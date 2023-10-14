90d783fb54cc91be9422e5c41deedfff5e9c9fee
root <root@gk3v-pb>
2023-10-14 22:22:22 +0200

    add GraphDumpFile and tinc-up script, copied from fw

--- modules/tinc-client.nix
+++ modules/tinc-client.nix
@@ -1,9 +1,10 @@
-{ config, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 let
   name       = "bbbsnowball";
   private    = "/etc/nixos/private/private";
   ifaceName  = builtins.substring 0 10 name;
-  tincIP     = (builtins.head config.networking.interfaces."tinc.${ifaceName}".ipv4.addresses).address;
+  #tincIP     = (builtins.head config.networking.interfaces."tinc.${ifaceName}".ipv4.addresses).address;
+  tincIPs    = map (x: "${x.address}/${toString x.prefixLength}") config.networking.interfaces."tinc.${ifaceName}".ipv4.addresses;
   hostName   = config.networking.hostName;
   ourName    = builtins.replaceStrings ["-"] ["_"] hostName;
 in
@@ -21,6 +22,10 @@ in
       LocalDiscovery=yes
       ConnectTo=sonline
       ConnectTo=routeromen
+
+      # tincd chroots into /etc/tinc/${name} so we cannot put the file into /run, as we usually would.
+      # Furthermore, tincd needs write access to the directory so we make a subdir.
+      GraphDumpFile = status/graph.dot
     '';
   };
 
@@ -49,7 +54,33 @@ in
       ${pkgs.rsync}/bin/rsync -r --delete ${private}/tinc-pubkeys/${name}/ /etc/tinc/${name}/hosts
     fi
 
+    ${pkgs.coreutils}/bin/install -d -o tinc.${name} -m700 /etc/tinc/${name}/status
+
     chmod 444 /etc/tinc/${name}/hosts/*
     chown -R tinc.${name} /etc/tinc/${name}/hosts/
   '';
+
+  # NixOS network config doesn't setup the interface if we restart the tinc daemon
+  # so let's add some redundancy:
+  environment.etc = lib.mkIf (builtins.length tincIPs > 0) {
+    "tinc/${name}/tinc-up" = {
+      text = ''
+        #!/bin/sh
+        #''${pkgs.nettools}/bin/ifconfig $INTERFACE ''${tinc-a-address} netmask 255.255.255.0
+        ${pkgs.iproute2}/bin/ip link set $INTERFACE up
+        for addr in ${toString tincIPs} ; do
+          ${pkgs.iproute2}/bin/ip address add $addr dev $INTERFACE
+        done
+      '';
+      mode = "755";
+    };
+    "tinc/${name}/tinc-down" = {
+      text = ''
+        #!/bin/sh
+        ''${pkgs.nettools}/bin/ifconfig $INTERFACE down
+        ${pkgs.iproute2}/bin/ip link set $INTERFACE down
+      '';
+      mode = "755";
+    };
+  };
 }
