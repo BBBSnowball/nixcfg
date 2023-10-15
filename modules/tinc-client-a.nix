e69bd14474c2ec6d8c1f11d96de4a22da5f0de3f
root <root@gk3v-pb>
2023-10-15 03:43:26 +0200

    bind resolv.conf into tinc daemon so it can resolve host names

--- modules/tinc-client-a.nix
+++ modules/tinc-client-a.nix
@@ -39,28 +39,34 @@ in
   # scp -3 gk3v:/etc/nixos/private/data/tinc-pubkeys/bbbsnowball/`hostname` nixos:/etc/nixos/private/tinc/pubkeys/bbbsnowball/`hostname`
   # scp -3 "root@nixos:/etc/nixos/private/tinc/pubkeys/bbbsnowball/*" root@`hostname`:/etc/nixos/private/data/tinc-pubkeys/bbbsnowball/
 
-  systemd.services."tinc.${name}".preStart = ''
-    ${pkgs.coreutils}/bin/install -o tinc.${name} -m755 -d /etc/tinc/${name}/hosts
+  systemd.services."tinc.${name}" = {
+    preStart = ''
+      ${pkgs.coreutils}/bin/install -o tinc.${name} -m755 -d /etc/tinc/${name}/hosts
+  
+      if [ -e "/etc/nixos/secret_local/tinc-${name}-rsa_key.priv" ] ; then
+        ${pkgs.coreutils}/bin/install -o root -m400 /etc/nixos/secret_local/tinc-${name}-rsa_key.priv /etc/tinc/${name}/rsa_key.priv
+      else
+        ${pkgs.coreutils}/bin/install -o root -m400 /etc/nixos/secret/tinc-${name}-rsa_key.priv /etc/tinc/${name}/rsa_key.priv
+      fi
+  
+      if [ -e "${private}/by-host/${hostName}/tinc-pubkeys/${name}" ] ; then
+        # We tell rsync to follow symlinks so the host-specific directory can refer to files in the common dir.
+        ${pkgs.rsync}/bin/rsync -r --delete -L "${private}/by-host/${hostName}/tinc-pubkeys/${name}/" /etc/tinc/${name}/hosts
+      else
+        #${pkgs.coreutils}/bin/install -o tinc.${name} -m444 ${private}/tinc-pubkeys/${name}/* /etc/tinc/${name}/hosts/
+        ${pkgs.rsync}/bin/rsync -r --delete ${private}/tinc-pubkeys/${name}/ /etc/tinc/${name}/hosts
+      fi
+  
+      ${pkgs.coreutils}/bin/install -d -o tinc.${name} -m700 /etc/tinc/${name}/status
+  
+      chmod 444 /etc/tinc/${name}/hosts/*
+      chown -R tinc.${name} /etc/tinc/${name}/hosts/
+    '';
 
-    if [ -e "/etc/nixos/secret_local/tinc-${name}-rsa_key.priv" ] ; then
-      ${pkgs.coreutils}/bin/install -o root -m400 /etc/nixos/secret_local/tinc-${name}-rsa_key.priv /etc/tinc/${name}/rsa_key.priv
-    else
-      ${pkgs.coreutils}/bin/install -o root -m400 /etc/nixos/secret/tinc-${name}-rsa_key.priv /etc/tinc/${name}/rsa_key.priv
-    fi
-
-    if [ -e "${private}/by-host/${hostName}/tinc-pubkeys/${name}" ] ; then
-      # We tell rsync to follow symlinks so the host-specific directory can refer to files in the common dir.
-      ${pkgs.rsync}/bin/rsync -r --delete -L "${private}/by-host/${hostName}/tinc-pubkeys/${name}/" /etc/tinc/${name}/hosts
-    else
-      #${pkgs.coreutils}/bin/install -o tinc.${name} -m444 ${private}/tinc-pubkeys/${name}/* /etc/tinc/${name}/hosts/
-      ${pkgs.rsync}/bin/rsync -r --delete ${private}/tinc-pubkeys/${name}/ /etc/tinc/${name}/hosts
-    fi
-
-    ${pkgs.coreutils}/bin/install -d -o tinc.${name} -m700 /etc/tinc/${name}/status
-
-    chmod 444 /etc/tinc/${name}/hosts/*
-    chown -R tinc.${name} /etc/tinc/${name}/hosts/
-  '';
+    serviceConfig.BindPaths = [
+      "/etc/resolv.conf:/etc/tinc/${name}/etc/resolv.conf"
+    ];
+  };
 
   # NixOS network config doesn't setup the interface if we restart the tinc daemon
   # so let's add some redundancy:
