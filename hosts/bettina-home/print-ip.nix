84fcd85c3df6a48b5126bb577302a4de65ab7ac5
Benjamin Koch <bbbsnowball@gmail.com>
2024-01-04 07:21:06 +0100

    bettina-home: improve print-ip scripts

--- hosts/bettina-home/print-ip.nix
+++ hosts/bettina-home/print-ip.nix
@@ -6,9 +6,10 @@
       onState = [ "routable" "off" ];
       script = ''
         #!${pkgs.runtimeShell}
-        PATH=${lib.makeBinPath (with pkgs; [ iproute2 jq inxi util-linux ])}:$PATH
-        logger "print-ip: DEBUG: $IFACE, $AdministrativeState, $ADDR"
+        PATH=${lib.makeBinPath (with pkgs; [ iproute2 jq util-linux systemd ])}:$PATH
+        logger "print-ip: DEBUG: $IFACE, $AdministrativeState, $OperationalState, $STATE, $ADDR, $IP_ADDRS"
         if [[ $IFACE != "lo" && $AdministrativeState == "configured" ]]; then
+        #if [[ $IFACE != "lo" ]]; then
           msg="$(
             echo "(print-ip via networkd-dispatcher)"
             echo ""
@@ -16,7 +17,11 @@
             ${pkgs.jq}/bin/jq -r '.[] | select(.ifname != "lo") |  select(.flags.[] | . == "UP") | .addr_info.[] | select(.family != "inet6" or (.temporary | not)) | (.local + " / " + (.prefixlen | tostring))'
             echo ""
             echo ""
-            echo "IP: $ADDR on $IFACE"
+            networkctl status -n0
+            echo ""
+            echo ""
+            echo "IP: $ADDR on $IFACE ($AdministrativeState), $IP_ADDRS"
+            echo ""
             echo ""
           )"
           logger "print-ip: IP: $ADDR on $IFACE"
@@ -31,27 +36,41 @@
   };
 
   systemd.services.print-ip-monitor = {
+    after = [ "getty.target" ];
     wantedBy = [ "network-pre.target" ];
-    path = with pkgs; [ iproute2 inxi util-linux ];
+    path = with pkgs; [ iproute2 util-linux ];
     script = ''
-      HOME=/root inxi -v3 --tty -c2 --ip >/dev/tty0
-      ip monitor address >/dev/tty0
+      ip -4 monitor address >/dev/tty0
+    '';
+  };
+
+  systemd.services.inxi = {
+    after = [ "getty.target" ];
+    wantedBy = [ "multi-user.target" ];
+    path = with pkgs; [ iproute2 inxi util-linux ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      sleep 10
+      HOME=/root inxi -v3 --tty -c2 --ip -Z >/dev/tty0
     '';
   };
 
   systemd.services.print-ip = {
-    after = [ "network.target" ];
+    after = [ "network.target" "getty.target" ];
     wantedBy = [ "multi-user.target" ];
     path = with pkgs; [ iproute2 jq inxi util-linux ];
+    serviceConfig.Type = "oneshot";
     script = ''
       msg="$(
         echo "(print-ip service)"
-        HOME=/root inxi --tty -c2 --ip
+        #HOME=/root inxi --tty -c2 --ip
         ip -j a s | \
         jq -r '.[] | select(.ifname != "lo") |  select(.flags.[] | . == "UP") | .addr_info.[] | select(.family != "inet6" or (.temporary | not)) | (.local + " / " + (.prefixlen | tostring))'
         echo ""
+        echo ""
       )"
       logger "print-ip: $msg"
+      sleep 5
       for tty in /dev/tty{0,1,2,3} ; do
         echo "$msg" >$tty
       done
