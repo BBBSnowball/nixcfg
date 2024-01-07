d076592d8bcb11b246b92c5de886eca7e5252e4a
Benjamin Koch <bbbsnowball@gmail.com>
2024-01-07 02:52:36 +0100

    bettina-home: use MongoDB 3.4 for Omada
    The newer one is not available on Hydra and it crashes on startup.
    This is not made available on the network so using an outdated
    version should be ok-ish.

--- hosts/bettina-home/mongodb-for-omada.nix
+++ hosts/bettina-home/mongodb-for-omada.nix
@@ -3,10 +3,10 @@
   # Hydra doesn't build it because SSPL has more restrictions than AGPL and the build takes for ages.
   # -> It's not the best idea to pin this but we don't have any other good option, I think.
   nixpkgs.overlays = [ (final: prev: {
-    #mongodb = nixpkgs-mongodb.legacyPackages.${pkgs.stdenv.hostPlatform.system}.mongodb;
-    mongodb = (import nixpkgs-mongodb {
-      config.allowUnfreePredicate = x: true;
-      system = pkgs.stdenv.hostPlatform.system;
-    }).mongodb;
+    mongodb = nixpkgs-mongodb.legacyPackages.${pkgs.stdenv.hostPlatform.system}.mongodb-3_4;
+    #mongodb = (import nixpkgs-mongodb {
+    #  config.allowUnfreePredicate = x: true;
+    #  system = pkgs.stdenv.hostPlatform.system;
+    #}).mongodb;
   }) ];
 }
