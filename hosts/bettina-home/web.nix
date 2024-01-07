149949d54f6f61034955b53bc6beeee1cbd57983
Benjamin Koch <bbbsnowball@gmail.com>
2024-01-07 22:44:44 +0100

    bettina-home: improve landing page

--- hosts/bettina-home/web.nix
+++ hosts/bettina-home/web.nix
@@ -5,6 +5,14 @@
     virtualHosts."default" = {
       #root = "/var/www/default";
       root = ./html;
+
+      locations."/index.html".extraConfig = ''
+        # only cache for a short time and only in memory
+        # so we don't need manual updates and we notice
+        # when network is down
+        expires 60s;
+        add_header Cache-Control "no-cache";
+      '';
     };
   };
 
