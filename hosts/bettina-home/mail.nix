a8b67d58e231251e7abb7a39056ce7917b914ea7
Benjamin Koch <bbbsnowball@gmail.com>
2024-01-25 02:26:09 +0100

    bettina-home: use msmtp daemon instead of setuid

--- hosts/bettina-home/mail.nix
+++ hosts/bettina-home/mail.nix
@@ -1,84 +1,72 @@
 { lib, pkgs, privateForHost, secretForHost, ... }:
-let
-  # msmtp wants us to use passwordeval (or GNOME keyring) to access the password
-  # but it uses popen, which goes through /bin/sh and kills the setuid rights.
-  usePasswordEval = false;
-in
 {
   programs.msmtp = {
     enable = true;
     setSendmail = true;
 
     # see https://wiki.archlinux.org/title/msmtp
-    extraConfig = let p = privateForHost.mail; in 
+    extraConfig = let p = privateForHost.mail; in
     ''
       defaults
-      auth           on
-      tls            on
-      tls_trust_file /etc/ssl/certs/ca-certificates.crt
-      #logfile        ~/.msmtp.log
       syslog         on
-      set_from_header on
-      # This causes error "envelope-from address is missing" although it should
-      # use the value of the `from` setting - at least that's how I understand
-      # the documentation. Anyway, the server will check it so we don't have
-      # to be too strict here.
-      #allow_from_override off
+      tls_trust_file /etc/ssl/certs/ca-certificates.crt
 
-      # Gmail
-      account        mail
-      host           ${p.smtpHost}
-      port           ${toString p.smtpPort}
+      account        local
+      host           localhost
+      port           25
       tls_starttls   off
       #NOTE You can set `from` to username+%U@domain to include the name of the user that is calling sendmail
       #     but keep in mind that this can be faked by setting $USER or $LOGNAME.
       from           ${p.from}
+      auth           off
+      tls            off
+      set_from_header on
+
+      # This causes error "envelope-from address is missing" although it should
+      # use the value of the `from` setting - at least that's how I understand
+      # the documentation. Anyway, the smarthost will check it so we don't have
+      # to be too strict here.
+      #allow_from_override off
+
+      account        smarthost
+      host           ${p.smtpHost}
+      port           ${toString p.smtpPort}
+      tls_starttls   off
+      tls            on
+      auth           on
       user           ${p.username}
-      passwordeval   cat /etc/msmtp-password
+      passwordeval   cat $CREDENTIALS_DIRECTORY/password
 
-      # Set a default account
-      account default: mail
+      # We won't be able to read the password as the sending user and setuid is unwanted
+      # and unreliable in services (e.g. PrivateDevices=true breaks it). Therefore, we
+      # pass the mail to our local msmtpd, which will forward it to the smarthost.
+      account default: local
     '';
   };
 
-  # default is to set this to root and don't setuid but we need setuid to access the password
-  services.mail.sendmailSetuidWrapper = {
-    setuid = lib.mkForce true;
-    setgid = lib.mkForce true;
-    owner = lib.mkForce "msmtp";
-    group = lib.mkForce "msmtp";
+  systemd.sockets.msmtp = {
+    description = "MSMTP daemon forwards mail to smarthost";
+    wantedBy = [ "sockets.target" ];
+    listenStreams = [ "127.0.0.1:25" ];
+    socketConfig.Accept = true;  # inetd mode
   };
 
-  users.users.msmtp = {
-    isSystemUser = true;
-    group = "msmtp";
+  systemd.services."msmtp@" = {
+    description = "MSMTP daemon (inetd)";
+
+    serviceConfig = {
+      # We tried to point it to a different config with $HOME and $SYSCFGDIR but that didn't work
+      # so we are left with the somewhat ugly `--command=...`.
+      # The "-" prefix is to ignore any errors because client errors are likely to cause a error exit value.
+      ExecStart = "-${pkgs.msmtp}/bin/msmtpd --inetd --log=syslog --command=\"${pkgs.msmtp}/bin/msmtp -f %%F --account=smarthost --\"";
+
+      StandardInput = "socket";
+      StandardError = "journal";
+
+      LoadCredential = [ "password:${secretForHost}/msmtp-password" ];
+
+      User = "msmtp";
+      DynamicUser = true;
+    };
   };
-  users.groups.msmtp = {};
-
-  systemd.services.msmtp-password = {
-    wantedBy = [ "network.target" "multi-user.target" ];
-    serviceConfig.Type = "oneshot";
-
-    serviceConfig.ExecStart = lib.mkIf usePasswordEval
-      ("${pkgs.coreutils}/bin/install -C -m 0400 -o msmtp -g msmtp "
-      + "${secretForHost}/msmtp-password /etc/msmtp-password");
-
-    script = lib.mkIf (!usePasswordEval) ''
-      # We cannot use passwordeval to read the password so we must bake it into the config file.
-      umask 077
-      t=$(mktemp)
-      while read line ; do
-        if [[ $line =~ passwordeval ]] ; then # very imprecise but input is known and trusted
-          echo "password $(cat ${secretForHost}/msmtp-password)"
-        else
-          echo "$line"
-        fi
-      done </etc/msmtprc-without-secrets >"$t"
-
-      install -C -m 0400 -o msmtp -g msmtp "$t" /etc/msmtprc
-      rm "$t"
-    '';
-  };
-
-  environment.etc."msmtprc".target = lib.mkIf (!usePasswordEval) "msmtprc-without-secrets";
 }
--- hosts/bettina-home/vaultwarden.nix
+++ hosts/bettina-home/vaultwarden.nix
@@ -6,29 +6,21 @@
     config = {
       EMAIL_CHANGE_ALLOWED = false;
       SIGNUPS_ALLOWED = false;
-      DOMAIN = "https://passwords-vpn.bettina-home.${privateForHost.domain}";
-      USE_SENDMAIL = true;
+      DOMAIN = "https://passwords.vpn.bettina-home.${privateForHost.domain}";
+
       SMTP_FROM = builtins.replaceStrings ["%U"] ["vaultwarden"] privateForHost.mail.from;
-      SMTP_DEBUG = true;  #FIXME remove
+      SMTP_HOST = "127.0.0.1";
+      SMTP_PORT = "25";
+      SMTP_SECURITY = "off";
+      #SMTP_DEBUG = "true";
     };
-    #environmentFile = "/run/credentials/vaultwarden.service/secret";
+
+    # Set ADMIN_TOKEN in vaultwarden.env. Hash it like this:
+    #   pwgen -AB0 20 5
+    #   nix-shell -p openssl libargon2 --run 'tr -d "\\r\\n" | argon2 "$(openssl rand -base64 32)" -e -id -k 65540 -t 3 -p 4'
+    #   (see https://github.com/dani-garcia/vaultwarden/wiki/Enabling-admin-page#using-argon2)
     environmentFile = "${secretForHost}/vaultwarden.env";
   };
-
-  # Set ADMIN_TOKEN in vaultwarden.env. Hash it like this:
-  #   pwgen -AB0 20 5
-  #   nix-shell -p openssl libargon2 --run 'tr -d "\\r\\n" | argon2 "$(openssl rand -base64 32)" -e -id -k 65540 -t 3 -p 4'
-  #   (see https://github.com/dani-garcia/vaultwarden/wiki/Enabling-admin-page#using-argon2)
-  #systemd.services.vaultwarden.serviceConfig.LoadCredential = [ "secret:${secretForHost}/vaultwarden.env" ];
-
-  # make sendmail available in the service
-  systemd.services.vaultwarden.path = [ "/run/wrappers/bin" "/run/wrappers" ];
-  #systemd.services.vaultwarden.serviceConfig.BindPaths = [
-  #  "/run/wrappers/bin/sendmail"
-  #  "/etc/msmtprc"
-  #];
-  # This breaks setuid for sendmail so we disable it.
-  systemd.services.vaultwarden.serviceConfig.PrivateDevices = lib.mkForce false;
 
   # create backup directory
   systemd.services.backup-vaultwarden.serviceConfig.ExecStartPre = "+${pkgs.coreutils}/bin/install "
