019e6318a3b47ce035647e2af00ee8597ee2ee03
root <root@gk3v-pb>
2024-01-27 19:47:42 +0100

    hosts/gk3v-pb/flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/9fb122519e9cd465d532f736a98c1e1eb541ef6f' (2023-11-16)
      → 'github:NixOS/nixpkgs/a77ab169a83a4175169d78684ddd2e54486ac651' (2024-01-24)

--- hosts/gk3v-pb/flake.nix
+++ hosts/gk3v-pb/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   inputs.routeromen.url = "github:BBBSnowball/nixcfg";
   #inputs.routeromen.url = "path:../..";
   inputs.routeromen.inputs.nixpkgs.follows = "nixpkgs";
