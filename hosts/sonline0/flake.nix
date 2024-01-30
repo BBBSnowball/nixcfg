1e62c07dbfab1292623dd3b2fa3524e84989e277
Benjamin Koch <bbbsnowball@gmail.com>
2024-01-30 03:53:57 +0100

    hosts/sonline0/flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/a08e061a4ee8329747d54ddf1566d34c55c895eb' (2023-05-09)
      → 'github:NixOS/nixpkgs/56911ef3403a9318b7621ce745f5452fb9ef6867' (2024-01-27)

--- hosts/sonline0/flake.nix
+++ hosts/sonline0/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   inputs.nixpkgs-dhclient.url = "github:NixOS/nixpkgs/nixos-21.11";
   inputs.routeromen.url = "github:BBBSnowball/nixcfg";
   inputs.routeromen.inputs.nixpkgs.follows = "nixpkgs";
