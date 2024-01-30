54114a0063110ee73a34b828fcfc43bc810dc608
Benjamin Koch <bbbsnowball@gmail.com>
2024-01-30 03:50:42 +0100

    hosts/c3pbvm/flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgsLetsmeet':
        'github:NixOS/nixpkgs/eabc38219184cc3e04a974fe31857d8e0eac098d' (2022-08-02)
      → 'github:NixOS/nixpkgs/70bdadeb94ffc8806c0570eb5c2695ad29f0e421' (2024-01-03)

--- hosts/c3pbvm/flake.nix
+++ hosts/c3pbvm/flake.nix
@@ -3,7 +3,7 @@
   inputs.flake-compat.follows = "routeromen/flake-compat";
   inputs.routeromen.url = "github:BBBSnowball/nixcfg";
   inputs.routeromen.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.nixpkgsLetsmeet.url = "github:NixOS/nixpkgs/nixos-21.11";
+  inputs.nixpkgsLetsmeet.url = "github:NixOS/nixpkgs/nixos-23.05";  # edumeet wants NodeJS 16
   inputs.chatgpt-telegram-bot.url = "github:BBBSnowball/chatgpt-telegram-bot";
   inputs.chatgpt-telegram-bot.inputs.nixpkgs.follows = "nixpkgs";
 
