a20df668597a92a3c5723edd2eb02f80c2f5de4c
Benjamin Koch <bbbsnowball@gmail.com>
2024-03-12 18:26:02 +0100

    nixosvm: use sets and maps for nftables

--- hosts/nixosvm/firewall-nftables.nix
+++ hosts/nixosvm/firewall-nftables.nix
@@ -11,18 +11,63 @@ in
   networking.nftables.tables.filter = {
     #family = "ip";
     family = "inet";
+
+    # for fhem:
+    #TODO The second rule is required for the first rule to work and we need a route on bbverl:
+    # route add -host 192.168.88.2 gw 192.168.84.37
+    #allow_port_forward(in_iface, "bbbsnowball-dev", "192.168.84.47", :tcp, 80)
+    #dnat_port_forward(in_iface, "192.168.85.47", :tcp, 80, "bbbsnowball-dev", "192.168.84.47", 80)
+
     content = ''
+      set allow_vpn_to_tinc {
+        typeof ip daddr . tcp dport
+        counter
+        flags constant
+        comment "allow traffic from Android VPN to these hosts+ports";
+        elements = {
+          # calendar
+          192.168.84.36 . 443,
+          # fhem
+          192.168.84.47 . 80
+        }
+      }
+
+      set allow_vpn_to_tinc_ips {
+        typeof ip daddr
+        counter
+        flags constant
+        comment "same as allow_vpn_to_tinc but only the IPs";
+        elements = {
+          192.168.84.36,
+          192.168.84.47
+        }
+      }
+
+      map dnat_vpn_to_tinc {
+        typeof ip saddr . ip daddr . tcp dport : ip daddr . tcp dport;
+        counter
+        flags constant, interval
+        comment "DNAT mappings for Android VPN";
+
+        elements = {
+          # fhem
+          192.168.88.0/23 . 192.168.85.47/32  . 80   : 192.168.84.47 . 80,
+          #
+          0.0.0.0/0       . 192.168.112.10/32 . 80   : ${upstreamIP} . ${toString ports.rss.port},
+          0.0.0.0/0       . 192.168.118.10/32 . 80   : ${upstreamIP} . ${toString ports.notes-magpie-ext.port},
+          #
+          # Dummy port, copied from old VPN on kim: 1743 on public IP of Kim is redirected to 443 on gallery for access to Davical/calendar
+          192.168.88.0/23 . 37.187.106.83/32  . 1743 : 192.168.84.36 . 443,
+          192.168.91.0/23 . 37.187.106.83/32  . 1743 : 192.168.84.36 . 443,
+        }
+      }
+
       chain filter {
         type filter hook forward priority filter; policy drop;
 
-        iifname "vpn_android-*" oifname "tinc.bbbsnowbal" ip daddr 192.168.84.36 tcp dport 443 accept
+        iifname "vpn_android-*" oifname "tinc.bbbsnowbal" ip daddr . tcp dport @allow_vpn_to_tinc accept
+        oifname "vpn_android-*" iifname "tinc.bbbsnowbal" ip saddr . tcp sport @allow_vpn_to_tinc accept
 
-        # calendar
-        iifname "vpn_android-*" oifname "tinc.bbbsnowbal" ip daddr 192.168.84.36 tcp dport 443 accept
-        oifname "vpn_android-*" iifname "tinc.bbbsnowbal" ip saddr 192.168.84.36 tcp sport 443 accept
-        # fhem
-        iifname "vpn_android-*" oifname "tinc.bbbsnowbal" ip daddr 192.168.84.47 tcp dport 80 accept
-        oifname "vpn_android-*" iifname "tinc.bbbsnowbal" ip saddr 192.168.84.47 tcp sport 80 accept
         iifname "vpn_*" oifname "ens3" ip daddr { 192.168.0.0/16, 127.0.0.0/8 } jump fw-reject
         iifname "vpn_*" oifname "ens3" accept
         oifname "vpn_*" iifname "ens3" accept
@@ -36,16 +81,8 @@ in
       chain nat2 {
         type nat hook prerouting priority dstnat;
 
-        #TODO The second rule is required for the first rule to work and we need a route on bbverl:
-        # route add -host 192.168.88.2 gw 192.168.84.37
-        #allow_port_forward(in_iface, "bbbsnowball-dev", "192.168.84.47", :tcp, 80)
-        #dnat_port_forward(in_iface, "192.168.85.47", :tcp, 80, "bbbsnowball-dev", "192.168.84.47", 80)
-        iifname "vpn_android-*" ip saddr 192.168.88.0/23 ip daddr 192.168.85.47/32 tcp dport 80 counter dnat to 192.168.84.47:80
-
-        iifname "vpn_android-*" ip daddr 192.168.112.10/32 tcp dport 80 counter dnat to ${upstreamIP}:${toString ports.rss.port}
-        iifname "vpn_android-*" ip daddr 192.168.118.10/32 tcp dport 80 counter dnat to ${upstreamIP}:${toString ports.notes-magpie-ext.port}
-        # Dummy port, copied from old VPN on kim: 1743 on public IP of Kim is redirected to 443 on gallery for access to Davical/calendar
-        iifname "vpn_android-*" ip saddr { 192.168.88.0/23, 192.168.91.0/23 } ip daddr 37.187.106.83/32 tcp dport 1743 counter dnat to 192.168.84.36:443
+        # lookup saddr+daddr+dport in map, get ip addr and port for dnat
+        iifname "vpn_android-*" dnat ip addr . port to ip saddr . ip daddr . tcp dport map @dnat_vpn_to_tinc
 
         #TODO We shoud properly filter incoming packets from VPN: deny from vpn_+ in INPUT, allow "--icmp-type destination-unreachable", whitelist appropriate ports
         #TODO This should already be rejected in FORWARD but this is not logged and connection times out instead.
@@ -58,7 +95,7 @@ in
         oifname "ens3" masquerade
 
         # adjust source IP so tinc can handle the packets
-        oifname "tinc.bbbsnowbal" ip saddr { 192.168.88.0/23, 192.168.91.0/23 } ip daddr { 192.168.84.36/32, 192.168.84.47/32 } masquerade
+        oifname "tinc.bbbsnowbal" ip saddr { 192.168.88.0/23, 192.168.91.0/23 } ip daddr @allow_vpn_to_tinc_ips masquerade
       }
     '';
   };
