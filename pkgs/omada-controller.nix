966198ea95effae959b8286ea03a94a795829217
root <root@gk3v-pb>
2024-04-22 00:49:50 +0200

    omada-controller: 5.13.23 -> 5.13.30.8

--- pkgs/omada-controller.nix
+++ pkgs/omada-controller.nix
@@ -2,13 +2,13 @@
 { stdenv, fetchzip, openjdk, mongodb }:
 stdenv.mkDerivation {
   pname = "omada-controller";
-  version = "5.13.23";
+  version = "5.13.30.8";
 
   src = fetchzip {
     # https://www.tp-link.com/de/support/download/omada-software-controller/
-    url = "https://static.tp-link.com/upload/software/2024/202401/20240112/Omada_SDN_Controller_v5.13.23_linux_x64.tar.gz";  # from the link above
+    url = "https://static.tp-link.com/upload/software/2024/202402/20240227/Omada_SDN_Controller_v5.13.30.8_linux_x64.tar.gz";  # from the link above
     #url = "https://download.tplinkcloud.com/firmware/Omada_SDN_Controller_v5.13.23_linux_x64_20231228194009_1705039261727.tar.gz";  # from webinterface of controller
-    hash = "sha256-Z+GGXZQ1R69L7Bkezy7QO6bIogm1HCch4WZAEcP7qT8=";
+    hash = "sha256-hLSyZ8L4S3fpo5wI7qFWKDnaaJ1SIxK+kNV3dFx5mmU=";
     stripRoot = false;
   };
 
