faa489f56a725b0e4badd7f0b95eb25853efafc9
Benjamin Koch <bbbsnowball@gmail.com>
2024-06-22 02:31:56 +0200

    ug1: upgrade to 24.05

--- hosts/ug1/flake.nix
+++ hosts/ug1/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs";
   inputs.nixpkgs-ollama.url = "github:NixOS/nixpkgs";
   inputs.routeromen.url = "github:BBBSnowball/nixcfg";
