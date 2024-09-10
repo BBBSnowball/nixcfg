d1f703b337d496d9382b443511fbce85b60bba32
Benjamin Koch <bbbsnowball@gmail.com>
2024-09-10 22:47:35 +0200

    fw: add lorri

--- hosts/framework/main.nix
+++ hosts/framework/main.nix
@@ -197,6 +197,8 @@ in
 
   hardware.glasgow.enable = true;
 
+  services.lorri.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
