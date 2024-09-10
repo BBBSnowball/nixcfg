89c8d097c89e3b705c3456ce2163b5510f85ccc8
Benjamin Koch <bbbsnowball@gmail.com>
2024-09-10 22:47:45 +0200

    fw: add tailscale client

--- hosts/framework/main.nix
+++ hosts/framework/main.nix
@@ -199,6 +199,8 @@ in
 
   services.lorri.enable = true;
 
+  services.tailscale.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
