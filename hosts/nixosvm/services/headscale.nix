cafc9a6d496aacd5228d0690953ac9f3e3f37003
Benjamin Koch <bbbsnowball@gmail.com>
2024-09-16 02:33:16 +0200

    nixosvm: headscale: keep only one DERP server
    There are several reasons:
    - Some derpprobe checks panick ("nil") and we have no idea whether
      this is an issue with derpprobe or our setup.
    - Thus, we have no good way to test DERP except in practice and that
      only really works with one DERP (because we cannot tell the client
      to use a specific one).
    - derpprobe runs mesh checks between pairs of DERPs. They fail (see
      above) and we haven't seen any sockets or packets between the DERPs,
      so we have no idea what firewall rules (and hairpinning) to add
      for this, i.e. meshing probably won't work in our setup.
    - The 2nd DERP was only to have one of them on the default port,
      which might help with tunnelling out of some weird network.
    - The 2nd DERP is behind a reverse proxy with TLS offloading.
      Apple clients seem to be unhappy with that for some reason and
      some Linux clients were regularly loosing there connection to DERP.
    
    => Single DERP seems to work fine and we can test it, so let's say
       that this is good enough, for now.

--- hosts/nixosvm/services/headscale.nix
+++ hosts/nixosvm/services/headscale.nix
@@ -39,13 +39,17 @@ in
 
   networking.firewall.allowedPorts.headscale = port;
 
+  systemd.services.headscale.restartTriggers = [
+    config.environment.etc."headscale/derp.yaml".source
+  ];
+
   environment.etc."headscale/derp.yaml".text = ''
     OmitDefaultRegions: true
     regions:
       900:
         regionid: 900
-        regioncode: custom
-        regionname: headscale
+        regioncode: sonline0
+        regionname: sonline0
         nodes:
           - name: 900a
             regionid: 900
@@ -55,15 +59,20 @@ in
             stunport: 3480
             stunonly: false
             derpport: 1443
-          - name: 900b
-            regionid: 900
-            hostname: derp1.${privateForHost.infoDomain}
-            ipv4: ${config.networking.externalIp}
-            ipv6: "${privateForHost.serverExternalIpv6}"
-            # coturn should work as a stun-only DERP.
-            stunport: 3478
-            stunonly: false
-            # ... and port 443 will be forwarded with TLS offloading.
-            derpport: 0
+      #901:
+      #  regionid: 901
+      #  regioncode: nginx
+      #  regionname: nginx
+      #  nodes:
+      #    - name: 901b
+      #      regionid: 901
+      #      hostname: derp1.${privateForHost.infoDomain}
+      #      ipv4: ${config.networking.externalIp}
+      #      #ipv6: "${privateForHost.serverExternalIpv6}"
+      #      # coturn should work as a stun-only DERP.
+      #      stunport: 3478
+      #      stunonly: false
+      #      # ... and port 443 will be forwarded with TLS offloading.
+      #      derpport: 0
   '';
 }
