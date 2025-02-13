62b353b323ce56137c8c9b0e2649eea7cf0db01b
Benjamin Koch <bbbsnowball@gmail.com>
2025-02-13 02:31:52 +0100

    routeromen: only start ffmpeg for Bambu stream when client is connected (ugly hack!)
    This is starting both services as user services but there are several
    issues with that:
    1. Nix won't automatically do a `systemctl daemon-reload` for the user.
    2. We have to run journalctl and systemctl as the user when debugging.
    3. It cannot access the secrets, so we have copied them to /tmp and
       given access to the service user.
    4. The node service doesn't need access to the secrets but now they are
       running under the same user, so it will have access.

--- hosts/routeromen/bambu.nix
+++ hosts/routeromen/bambu.nix
@@ -1,32 +1,52 @@
-{ pkgs, secretForHost, ... }:
+{ pkgs, config, secretForHost, ... }:
 let
   node-media-server = import ./node-media-server { inherit pkgs; };
   injectpassword = import ./injectpassword { inherit pkgs; };
 in
 {
-  systemd.services.node-media-server = {
+  systemd.user.services.node-media-server = let
+    userService = "user@${toString config.users.users.node-media-server.uid}.service";
+  in {
+    unitConfig.ConditionUser = "node-media-server";
+    # https://unix.stackexchange.com/a/590958
+    #bindsTo = [ userService ];
+    #after = [ "network.target" userService ];
+    # -> We still cannot use `systemctl --user`.
+    # -> Run both services as user services.
+
     after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
+    #wantedBy = [ "multi-user.target" ];
+    wantedBy = [ "default.target" ];
     path = with pkgs; [ node-media-server ];
     serviceConfig = {
       ExecStart = "${node-media-server}/bin/my-node-media-server";
       Restart = "always";
       RestartSec = 30;
-      DynamicUser = true;
-      User = "node-media-server";
+      #DynamicUser = true;
+      #User = "node-media-server";
     };
   };
 
-  systemd.services.stream-printer1 = {
+  users.users.node-media-server = {
+    isSystemUser = true;
+    group = "node-media-server";
+    #uid = 5859;  # must be known in advance
+    linger = true;
+  };
+  users.groups.node-media-server = {};
+
+  systemd.user.services.stream-printer1 = {
+    unitConfig.ConditionUser = "node-media-server";
     after = [ "network.target" "node-media-server.service" ];
-    wantedBy = [ "multi-user.target" ];
+    #wantedBy = [ "multi-user.target" ];
     path = with pkgs; [ ffmpeg iputils nmap ];
     serviceConfig = {
       Restart = "always";
       RestartSec = 30;
-      EnvironmentFile = "${secretForHost}/bambu";  # defines IP and ACCESS_CODE
-      DynamicUser = true;
-      User = "stream-printer1";
+      #EnvironmentFile = "${secretForHost}/bambu";  # defines IP and ACCESS_CODE
+      EnvironmentFile = "/tmp/bambu";  # defines IP and ACCESS_CODE
+      #DynamicUser = true;
+      #User = "stream-printer1";
     };
     script = ''
       INPUT="rtsps://bblp:$ACCESS_CODE@$IP:322/streaming/live/1"
--- hosts/routeromen/node-media-server/bin/app.js
+++ hosts/routeromen/node-media-server/bin/app.js
@@ -4,9 +4,12 @@
 
 const fs = require("fs");
 const path = require("path");
+const { spawn } = require('node:child_process');
 const config = require("./config.json");
 //const NodeMediaServer = require("..");
 const NodeMediaServer = require("node-media-server");
+const BroadcastServer = require("node-media-server/src/server/broadcast_server.js");
+const logger = require( "node-media-server/src/core/logger.js");
 
 if (config.rtmps?.key && !fs.existsSync(config.rtmps.key)) {
   config.rtmps.key = path.join(__dirname, config.rtmps.key);
@@ -24,5 +27,48 @@ if (config.https?.cert && !fs.existsSync(config.https.cert)) {
   config.https.cert = path.join(__dirname, config.https.cert);
 }
 
+class MyBroadcastServer extends BroadcastServer {
+  constructor() {
+    super();
+
+    this.postPlaySuper = this.postPlay;
+    this.postPlay = this.postPlay2;
+    this.donePlaySuper = this.donePlay;
+    this.donePlay = this.donePlay2;
+
+    this.streamActive = false;
+  }
+
+  // must be an arrow method to override
+  // https://stackoverflow.com/questions/45881670/should-i-write-methods-as-arrow-functions-in-angulars-class/45882417#45882417
+  // -> We cannot use super in an arrow method, so we use a different name and save the previous one before replacing it (see above).
+  postPlay2 = (session) => {
+    logger.info(`postPlay in MyBroadcastServer, protocol ${session.protocol}, before ${this.subscribers.size} subscribers`);
+    this.postPlaySuper(session);
+
+    if (!this.streamActive) {
+      logger.info("Starting stream service");
+      this.streamActive = true;
+      spawn("systemctl start --user stream-printer1.service", { stdio: "inherit", shell: true });
+    }
+  }
+  donePlay2 = (session) => {
+    this.donePlaySuper(session);
+    logger.info(`donePlay in MyBroadcastServer, protocol ${session.protocol}, after ${this.subscribers.size} subscribers`);
+    if (this.subscribers.size == 0 && this.streamActive) {
+      logger.info("Stopping stream service");
+      spawn("systemctl stop --user stream-printer1.service", { stdio: "inherit", shell: true });
+      this.streamActive = false;
+    }
+  }
+}
+
 const nms = new NodeMediaServer(config);
+
+const ctx = nms.ctx;
+
+const streamPath = "/live/test";
+nms.ctx.broadcasts.set(streamPath, new MyBroadcastServer());
+logger.info(`We have added MyBroadcastServer for ${streamPath}`);
+
 nms.run();
