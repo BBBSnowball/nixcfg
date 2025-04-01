707448f308fa09d4c363eaedc672cd7e39236fbc
Benjamin Koch <bbbsnowball@gmail.com>
2025-04-01 22:12:04 +0200

    nixosvm, c: omit HTTP error 401 from logs

--- hosts/nixosvm/containers/c.nix
+++ hosts/nixosvm/containers/c.nix
@@ -49,7 +49,8 @@ in {
         # omit success (and redirect) in logs unless they generate an absurd amount of load
         map $status,$request_time $loggable {
           ~,[^0]  1;  # slow
-          ~^[23]  0;  # successful (or redirect)
+          ~^[23]  0;  # successful (or redirect, not modified, etc)
+          401 0;      # unauthorized, not unusual because we have pw auth
           default 1;  # some unusual status
         }
         log_format mini '[$time_iso8601] $status $request_method "$host$uri" rt=$request_time';
