8e99678cae0213786b190aff91427ba60fb1ba29
root <root@gk3v-pb>
2025-05-02 18:53:41 +0200

    gk3v: upgrade Omada Controller v5.15.20.18 -> v5.15.20.20

--- pkgs/omada-controller.nix
+++ pkgs/omada-controller.nix
@@ -6,12 +6,14 @@ stdenv.mkDerivation {
 
   src = fetchzip {
     # https://www.tp-link.com/de/support/download/omada-software-controller/
-    url = "https://static.tp-link.com/upload/software/2025/202503/20250331/Omada_SDN_Controller_v5.15.20.18_linux_x64.tar.gz";  # from the link above
-    #url = "https://download.tplinkcloud.com/firmware/Omada_SDN_Controller_v5.13.23_linux_x64_20231228194009_1705039261727.tar.gz";  # from webinterface of controller
-    hash = "sha256-YtBdsBZmfydOeubNGXGaf1sQwm6/vWTvSGR+5FZfRY8=";
+    # https://support.omadanetworks.com/en/product/omada-software-controller/?resourceType=download  (English download page, seems to be more up-to-date)
+    url = "https://static.tp-link.com/upload/software/2025/202504/20250425/Omada_SDN_Controller_v5.15.20.20_linux_x64_20250416110546.tar.gz";  # from the link above
+    # Not available on TP Link website, yet, it seems -> only available on the English page:
+    #url = "https://download.tplinkcloud.com/firmware/Omada_SDN_Controller_v5.15.20.20_linux_x64_20250416110546_1745551413090.tar.gz";  # from webinterface of controller
+    hash = "sha256-4iLoERHpTuUe6feHWp258KtOw2XPWUyWbaQFcpIRNYk=";
     stripRoot = false;
 
-    #NOTE v5.15.20: I had to update permissions on one host: chmod u+w /var/lib/omada-controller/properties/omada.properties
+    #NOTE v5.15.20: I had to update permissions on both hosts: chmod u+w /var/lib/omada-controller/properties/omada.properties
   };
 
   buildInputs = [ openjdk mongodb ];
