03c4f12350f69d60860e44174bae5794c7a307f7
Benjamin Koch <bbbsnowball@gmail.com>
2025-05-02 19:49:02 +0200

    hosts/ug1/flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'lanzaboote':
        'github:nix-community/lanzaboote/64b903ca87d18cef2752c19c098af275c6e51d63' (2023-04-29)
      → 'github:nix-community/lanzaboote/a65905a09e2c43ff63be8c0e86a93712361f871e' (2025-01-23)
    • Updated input 'lanzaboote/crane':
        'github:ipetkov/crane/0c9f468ff00576577d83f5019a66c557ede5acf6' (2023-04-11)
      → 'github:ipetkov/crane/ef80ead953c1b28316cc3f8613904edc2eb90c28' (2024-11-08)
    • Removed input 'lanzaboote/crane/flake-compat'
    • Removed input 'lanzaboote/crane/flake-utils'
    • Removed input 'lanzaboote/crane/nixpkgs'
    • Removed input 'lanzaboote/crane/rust-overlay'
    • Updated input 'lanzaboote/flake-compat':
        'github:edolstra/flake-compat/35bb57c0c8d8b62bbfd284272c928ceb64ddbde9' (2023-01-17)
      → 'github:edolstra/flake-compat/0f9255e01c2351cc7d116c072cb317785dd33b33' (2023-10-04)
    • Updated input 'lanzaboote/flake-parts':
        'github:hercules-ci/flake-parts/dcc36e45d054d7bb554c9cdab69093debd91a0b5' (2023-04-01)
      → 'github:hercules-ci/flake-parts/506278e768c2a08bec68eb62932193e341f55c90' (2024-11-01)
    • Removed input 'lanzaboote/flake-utils'
    • Removed input 'lanzaboote/flake-utils/systems'
    • Updated input 'lanzaboote/pre-commit-hooks-nix':
        'github:cachix/pre-commit-hooks.nix/d3de8f69ca88fb6f8b09e5b598be5ac98d28ede5' (2023-04-13)
      → follows ''
    • Removed input 'lanzaboote/pre-commit-hooks-nix/flake-compat'
    • Removed input 'lanzaboote/pre-commit-hooks-nix/flake-utils'
    • Removed input 'lanzaboote/pre-commit-hooks-nix/gitignore'
    • Removed input 'lanzaboote/pre-commit-hooks-nix/gitignore/nixpkgs'
    • Removed input 'lanzaboote/pre-commit-hooks-nix/nixpkgs'
    • Removed input 'lanzaboote/pre-commit-hooks-nix/nixpkgs-stable'
    • Updated input 'lanzaboote/rust-overlay':
        'github:oxalica/rust-overlay/2c417c0460b788328220120c698630947547ee83' (2023-04-22)
      → 'github:oxalica/rust-overlay/0be641045af6d8666c11c2c40e45ffc9667839b5' (2024-11-18)
    • Removed input 'lanzaboote/rust-overlay/flake-utils'

--- hosts/ug1/flake.nix
+++ hosts/ug1/flake.nix
@@ -7,8 +7,9 @@
   inputs.flake-registry.url = "github:NixOS/flake-registry";
   inputs.flake-registry.flake = false;
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware";
-  inputs.lanzaboote.url = "github:nix-community/lanzaboote/v0.3.0";
+  inputs.lanzaboote.url = "github:nix-community/lanzaboote/v0.4.2";
   inputs.lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.lanzaboote.inputs.pre-commit-hooks-nix.follows = "";
   inputs.disko.url = "github:nix-community/disko/v1.6.1";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
 
