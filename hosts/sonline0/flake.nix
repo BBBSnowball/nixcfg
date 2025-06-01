48168aea3369e566035e389136132b4dc05a1cd1
root <root@sonline0>
2025-06-01 18:46:46 +0200

    sonline0: update to 25.05

--- hosts/sonline0/flake.nix
+++ hosts/sonline0/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   inputs.nixpkgs-dhclient.url = "github:NixOS/nixpkgs/nixos-21.11";
   inputs.routeromen.url = "github:BBBSnowball/nixcfg";
   inputs.routeromen.inputs.nixpkgs.follows = "nixpkgs";
