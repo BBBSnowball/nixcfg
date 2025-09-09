85560ab18ed92036138c4ed3a414c1b9c05b223f
Benjamin Koch <bbbsnowball@gmail.com>
2025-09-09 04:56:37 +0200

    nixos/omas: clean up update script for Wordpress packages

--- hosts/nixosvm/containers/wordpress/generate.sh
+++ hosts/nixosvm/containers/wordpress/generate.sh
@@ -15,16 +15,13 @@ echo "Generating for Wordpress $WP_VERSION ..."
 export PLUGINS="`< wordpress-plugins.json jq -r 'keys|join(",")'`"
 export THEMES="`< wordpress-themes.json jq -r 'keys|join(",")'`"
 
-#cd "$(dirname "$0")"
 t="$(umask 077; mktemp -d)"
+# Does wp4nix use the existing values, e.g. don't download again when version hasn't changed?
+# We don't know but it seems to read the existing files so let's make them available.
 cp {plugins,themes,pluginLanguages,themeLanguages}.json "$t/" || true
 chown -R generate-files "$t"
-#ln -s "$PWD"/wordpress-{plugins,themes}.json "$t/"
-#sudo -u generate-files -D "$t" WP_VERSION="$WP_VERSION" "$PWD/generate.sh"
-cmd="$PWD/generate-inner.sh"
-#( set -x; cd "$t" && sudo -u generate-files WP_VERSION="$WP_VERSION" PLUGINS="$PLUGINS" THEMES="$THEMES" PATH="$PATH" bash "$cmd" )
-#( set -x; cd "$t" && sudo -u generate-files --preserve-env=WP_VERSION,PLUGINS,THEMES,PATH bash "$cmd" )
 ( set -x; cd "$t" && sudo -u generate-files --preserve-env=WP_VERSION,PATH "`which wp4nix`" -p $PLUGINS -pl en,de,de_DE -t $THEMES -tl en,de,de_DE )
 rm -f {plugins,themes,pluginLanguages,themeLanguages}.json
 cp -L "$t"/{plugins,themes,pluginLanguages,themeLanguages}.json .
+sudo -u generate-files rm -rf "$t"
 
