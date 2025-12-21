99d2ab1f4b4f091833103a6edb4708d0af119297
Benjamin Koch <bbbsnowball@gmail.com>
2025-12-22 00:07:03 +0100

    ug1: upgrade to NixOS 25.11

--- hosts/ug1/flake.nix
+++ hosts/ug1/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs";
   inputs.nixpkgs-ollama.url = "github:NixOS/nixpkgs";
   inputs.routeromen.url = "github:BBBSnowball/nixcfg";
@@ -7,7 +7,7 @@
   inputs.flake-registry.url = "github:NixOS/flake-registry";
   inputs.flake-registry.flake = false;
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware";
-  inputs.lanzaboote.url = "github:nix-community/lanzaboote/v0.4.2";
+  inputs.lanzaboote.url = "github:nix-community/lanzaboote/v1.0.0";
   inputs.lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
   inputs.lanzaboote.inputs.pre-commit-hooks-nix.follows = "";
   inputs.disko.url = "github:nix-community/disko/v1.6.1";
--- hosts/ug1/ugreen.nix
+++ hosts/ug1/ugreen.nix
@@ -1,6 +1,12 @@
 { ... }:
 {
-  systemd.watchdog.runtimeTime = "1min";
-  systemd.watchdog.kexecTime = "10min";
-  systemd.watchdog.rebootTime = "10min";
+  #systemd.watchdog.runtimeTime = "1min";
+  #systemd.watchdog.kexecTime = "10min";
+  #systemd.watchdog.rebootTime = "10min";
+
+  systemd.settings.Manager = {
+    RuntimeWatchdogSec = "1min";
+    KExecWatchdogSec = "10min";
+    RebootWatchdogSec = "10min";
+  };
 }
