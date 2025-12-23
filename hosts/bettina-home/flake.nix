76982539c717e190691c5d2f056f84f846d76e32
root@bettina-home <root@bettina-home>
2025-12-23 18:03:21 +0100

    hosts/bettina-home/flake.lock: Update (NixOS 25.11)
    Flake lock file updates:
    
    • Updated input 'flake-registry':
        'github:NixOS/flake-registry/3f641cbae15d3c74370aa9b97fd0ac478a114305?narHash=sha256-erTXdDToRA8whxURoEgBGWj550vcUirO6adEFIjQ0M0%3D' (2023-08-23)
      → 'github:NixOS/flake-registry/cb70c9306b44501de412649c356dee503a25f119?narHash=sha256-q2jzJQdsJMpD3dbuNphQJgwx6XeGPonWOp43U0nY7o0%3D' (2025-11-19)
    • Updated input 'nixos-hardware':
        'github:NixOS/nixos-hardware/9a049b4a421076d27fee3eec664a18b2066824cb?narHash=sha256-fbWE4Xpw6eH0Q6in%2BymNuDwTkqmFmtxcQEmtRuKDTTk%3D' (2025-04-14)
      → 'github:NixOS/nixos-hardware/9154f4569b6cdfd3c595851a6ba51bfaa472d9f3?narHash=sha256-ZlJTNLUKQRANlLDomuRWLBCH5792x%2B6XUJ4YdFRjtO4%3D' (2025-11-29)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/a6c3a6141ec1b367c58ead3f7f846c772a25f4e5?narHash=sha256-vS6LSOMDOB3s%2BL6tqw9IGujxnmUAZQnEG%2BVi640LayI%3D' (2025-12-22)
      → 'github:NixOS/nixpkgs/b3aad468604d3e488d627c0b43984eb60e75e782?narHash=sha256-eplAP%2BrorKKd0gNjV3rA6%2B0WMzb1X1i16F5m5pASnjA%3D' (2025-12-20)
    • Added input 'nixpkgs-mongodb':
        'github:NixOS/nixpkgs/c6f52ebd45e5925c188d1a20119978aa4ffd5ef6?narHash=sha256-m5KWt1nOm76ILk/JSCxBM4MfK3rYY7Wq9/TZIIeGnT8%3D' (2025-12-15)
    • Updated input 'nixpkgs-unstable':
        'github:NixOS/nixpkgs/8473e133e19d4711242d6a17e7c15995fa2234a6?narHash=sha256-6H3OI3Oj3UzHAAwIV3V1TB3FuRcG/DZtMIw4HlhdqGc%3D' (2023-05-12)
      → 'github:NixOS/nixpkgs/0430acab84a83184c8685b7d715910489c8f9557?narHash=sha256-5R9/7njox/hWlMfaOJkxvARdQA3baze2870UGGD3vGc%3D' (2025-12-23)
    • Updated input 'routeromen':
        'github:BBBSnowball/nixcfg/bc3eb4f3664b223e62ba677e3955466b1250c55d?narHash=sha256-2TPsP94kM4NXSQrmLIoU%2BHL/RkDihLnubUoysAESrN4%3D' (2024-01-29)
      → 'github:BBBSnowball/nixcfg/16c808eb23a5f229334482cf050fc66ace477ed4?narHash=sha256-FGGFomD3AuTQBrLQh3W7H3ywl6LQI2hlRUR4zgh/Zhc%3D' (2025-11-26)
    • Removed input 'routeromen/nixpkgs-mongodb'
    • Updated input 'routeromen/nixpkgs-unstable':
        'github:NixOS/nixpkgs/ee176b5fbb680864dcf80f9ca3f6f134d81331d8?narHash=sha256-TR02XPre2EcwU1kT4WXZWJOBi/B8C7P1xoywOUGkdDk%3D' (2023-10-13)
      → 'github:NixOS/nixpkgs/8f85b83ec497f83ad2d5aeb0acedd7bf34062e44?narHash=sha256-jnD5nNeEBGIhAelhZRcBX6wjPm1NwCwl7ydT3UvkWNE%3D' (2024-12-20)

--- hosts/bettina-home/flake.nix
+++ hosts/bettina-home/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs";
   inputs.routeromen.url = "github:BBBSnowball/nixcfg";
   inputs.routeromen.inputs.nixpkgs.follows = "nixpkgs";
@@ -8,6 +8,7 @@
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware";
   inputs.disko.url = "github:nix-community/disko/v1.2.0";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nixpkgs-mongodb.url = "github:NixOS/nixpkgs/c6f52ebd45e5925c188d1a20119978aa4ffd5ef6";  # update triggers long rebuild
 
   outputs = { self, nixpkgs, routeromen, ... }@flakeInputs:
     routeromen.lib.mkFlakeForHostConfig "bettina-home" "x86_64-linux" ./main.nix flakeInputs;
