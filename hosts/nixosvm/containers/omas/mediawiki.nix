ed836b36ed0c384bbf39bd8409cc9215e527f50b
Benjamin Koch <bbbsnowball@gmail.com>
2026-01-25 18:25:37 +0100

    nixos/omas: update MediaWiki plugins

--- hosts/nixosvm/containers/omas/mediawiki.nix
+++ hosts/nixosvm/containers/omas/mediawiki.nix
@@ -147,50 +147,61 @@ in
         # -> too new
         #url = "https://extdist.wmflabs.org/dist/extensions/LDAPAuthentication2-REL1_42-05febf7.tar.gz";
         #hash = "sha256-OcKE2TUQHxPJnPwl00mS5v4czXbsniagiWHOOOO57EE=";
-        url = "https://extdist.wmflabs.org/dist/extensions/LDAPAuthentication2-REL1_39-1adda11.tar.gz";
-        hash = "sha256-dSuGGUYtFjzqWMgkycz7j9gzLxSGueX8U6NWyeiT8eo=";
+        #url = "https://extdist.wmflabs.org/dist/extensions/LDAPAuthentication2-REL1_39-1adda11.tar.gz";
+        #hash = "sha256-dSuGGUYtFjzqWMgkycz7j9gzLxSGueX8U6NWyeiT8eo=";
+        url = "https://extdist.wmflabs.org/dist/extensions/LDAPAuthentication2-REL1_43-0795e38.tar.gz";
+        hash = "sha256-q9mycwtoPhVwduryuGNlOAl5G6qkdvmFNQnfEmruun8=";
       };
       LDAPProvider = pkgs.fetchzip {
         # https://www.mediawiki.org/wiki/Extension:LDAPProvider
         #url = "https://extdist.wmflabs.org/dist/extensions/LDAPProvider-REL1_43-7febfb9.tar.gz";
         #hash = "sha256-GmJnxGss7ZA0ClVigupJ5z28PmRFHMveBKWWhecI5Tk=";
-        url = "https://extdist.wmflabs.org/dist/extensions/LDAPProvider-REL1_39-afc8861.tar.gz";
-        hash = "sha256-XLVXJAmvMHZtYSiRFXds0/4jKRtK3dkGAkJJ58Uilfw=";
+        #url = "https://extdist.wmflabs.org/dist/extensions/LDAPProvider-REL1_39-afc8861.tar.gz";
+        #hash = "sha256-XLVXJAmvMHZtYSiRFXds0/4jKRtK3dkGAkJJ58Uilfw=";
+        url = "https://extdist.wmflabs.org/dist/extensions/LDAPProvider-REL1_43-35ead31.tar.gz";
+        hash = "sha256-qXmfU3MrlztdxZZ7idwL+nzByIcXhVFg5SWfTVpvKIA=";
       };
       LDAPAuthorization = pkgs.fetchzip {
         # https://www.mediawiki.org/wiki/Extension:LDAPAuthorization
         #url = "https://extdist.wmflabs.org/dist/extensions/LDAPAuthorization-REL1_42-81bcaf5.tar.gz";
         #hash = "sha256-SzWqltXymsqS307fllx4HJlLfqynWDCwBL+xzy3vS+0=";
-        url = "https://extdist.wmflabs.org/dist/extensions/LDAPAuthorization-REL1_39-4ad821f.tar.gz";
-        hash = "sha256-z97RfEy3x/UrEGdzS3Zui9jw21IQV2jN2NT9kId6zAo=";
+        #url = "https://extdist.wmflabs.org/dist/extensions/LDAPAuthorization-REL1_39-4ad821f.tar.gz";
+        #hash = "sha256-z97RfEy3x/UrEGdzS3Zui9jw21IQV2jN2NT9kId6zAo=";
+        url = "https://extdist.wmflabs.org/dist/extensions/LDAPAuthorization-REL1_43-9c0788f.tar.gz";
+        hash = "sha256-8ZSpEPMuqHDfzUQwpS0sNj9Mmmp3ZnN97u5ww4KzU4U=";
       };
       LDAPUserInfo = pkgs.fetchzip {
         # https://www.mediawiki.org/wiki/Extension:LDAPUserInfo
         #url = "https://extdist.wmflabs.org/dist/extensions/LDAPUserInfo-REL1_42-f4cd072.tar.gz";
         #hash = "sha256-PmHZopUTv7vbWU53IxFY/gY0kwQbZm0QlPC6DExm2N4=";
-        url = "https://extdist.wmflabs.org/dist/extensions/LDAPUserInfo-REL1_39-361a8e5.tar.gz";
-        hash = "sha256-HhNzvxPAUFO+uLxGZ7FZOuCoa6ZU0BHTNJ2KnPKqkq0=";
+        #url = "https://extdist.wmflabs.org/dist/extensions/LDAPUserInfo-REL1_39-361a8e5.tar.gz";
+        #hash = "sha256-HhNzvxPAUFO+uLxGZ7FZOuCoa6ZU0BHTNJ2KnPKqkq0=";
+        url = "https://extdist.wmflabs.org/dist/extensions/LDAPUserInfo-REL1_43-7913cd2.tar.gz";
+        hash = "sha256-6OW99o94baqRplm/K2zoIOUWUevpFkE+2Ss1cSVvzRE=";
       };
       PluggableAuth = pkgs.fetchzip {
         # https://www.mediawiki.org/wiki/Extension:PluggableAuth
-        url = "https://extdist.wmflabs.org/dist/extensions/PluggableAuth-REL1_42-894ee32.tar.gz";
-        hash = "sha256-YHUek3pEM9XH2YLLkbfqEbPHMJBEzKGmJEin7eW91/g=";
+        #url = "https://extdist.wmflabs.org/dist/extensions/PluggableAuth-REL1_42-894ee32.tar.gz";
+        #hash = "sha256-YHUek3pEM9XH2YLLkbfqEbPHMJBEzKGmJEin7eW91/g=";
+        url = "https://extdist.wmflabs.org/dist/extensions/PluggableAuth-REL1_44-85e96ac.tar.gz";
+        hash = "sha256-Hjol18Aqm6cR6Aj7PXN13JOFLoQAa8WiJVDU1R73jsU=";
       };
       LDAPSyncAll = pkgs.fetchzip {
         # https://www.mediawiki.org/wiki/Extension:LDAPSyncAll
         #url = "https://extdist.wmflabs.org/dist/extensions/LDAPSyncAll-REL1_42-2d637e5.tar.gz";
         #hash = "sha256-6xE9Xx7u6i+JuejyrE7WzGWRQGDVIByHmzjKI7vHy0Q=";
-        url = "https://extdist.wmflabs.org/dist/extensions/LDAPSyncAll-REL1_39-843fd4b.tar.gz";
-        hash = "sha256-bLGv79n1dqUpq2XyMUbJckfo1l/VjyuyfTIsyKRrnpg=";
+        #url = "https://extdist.wmflabs.org/dist/extensions/LDAPSyncAll-REL1_39-843fd4b.tar.gz";
+        #hash = "sha256-bLGv79n1dqUpq2XyMUbJckfo1l/VjyuyfTIsyKRrnpg=";
+        url = "https://extdist.wmflabs.org/dist/extensions/LDAPSyncAll-REL1_43-e273b29.tar.gz";
+        hash = "sha256-JgRo93sNt69B6cB0wuqSq/oMlu5jFMNiXqN0uCLC/A8=";
       };
       LDAPGroups = pkgs.fetchzip {
         # https://www.mediawiki.org/wiki/Extension:LDAPSyncAll
-        url = "https://extdist.wmflabs.org/dist/extensions/LDAPGroups-REL1_39-1d5f4e9.tar.gz";
-        hash = "sha256-7alH76MrUxHg657b4oAfNHKcia9KnJ+/0DJRKpYMD1s=";
+        #url = "https://extdist.wmflabs.org/dist/extensions/LDAPGroups-REL1_39-1d5f4e9.tar.gz";
+        #hash = "sha256-7alH76MrUxHg657b4oAfNHKcia9KnJ+/0DJRKpYMD1s=";
+        url = "https://extdist.wmflabs.org/dist/extensions/LDAPGroups-REL1_43-f3fa6ef.tar.gz";
+        hash = "sha256-9WMmCe/X26/UsvyPpg/8+ofKIVWOwsiIYPfrKI/tKOI=";
       };
-
-
-      #FIXME LDAPGroups?
     };
   };
 
